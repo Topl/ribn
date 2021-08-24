8f9bae61f0573905e37f281dbb0facffdf5f36c1
Mustafa Ali <mustafaali.py@gmail.com>
2021-08-24 13:02:30 -0500

    Set up routing

---
+++ lib/constants/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const welcome = "/welcome";
+  static const selectAction = "/select-action";
+  static const createPassword = "/create-password";
+  static const seedPhrase = "/seed-phrase";
+  static const seedPhraseConfirm = "/seed-phrase-confirm";
+  static const login = "/login";
+  static const home = "/home";
+}
