d396286b1ccd20a5d3be126c125b34c717ecc548
Mustafa Ali <mustafaali.py@gmail.com>
2021-08-25 15:42:19 -0500

    Add example state models

---
+++ lib/models/app_state.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:ribn/models/onboarding_state.dart';
+
+@immutable
+class AppState {
+  final OnboardingState onboardingState;
+  const AppState({
+    required this.onboardingState,
+  });
+
+  factory AppState.initial() {
+    return AppState(onboardingState: OnboardingState.initial());
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is AppState && other.onboardingState == onboardingState;
+  }
+
+  @override
+  int get hashCode => onboardingState.hashCode;
+
+  AppState copyWith({
+    OnboardingState? onboardingState,
+  }) {
+    return AppState(
+      onboardingState: onboardingState ?? this.onboardingState,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'onboardingState': onboardingState.toMap(),
+    };
+  }
+
+  factory AppState.fromMap(Map<String, dynamic> map) {
+    return AppState(
+      onboardingState: OnboardingState.fromMap(map['onboardingState']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AppState.fromJson(String source) => AppState.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'AppState(onboardingState: $onboardingState)';
+}
