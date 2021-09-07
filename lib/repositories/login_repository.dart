0f5b9f41369b0cffc6bd507fa603dbe2f6379221
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-07 15:19:13 -0500

    Allow decrypting keystore to get topl

---
+++ lib/repositories/login_repository.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: implementation_imports
+import 'dart:typed_data';
+import 'package:bip_topl/bip_topl.dart';
+import 'package:mubrambl/src/crypto/keystore.dart';
+
+class LoginRepository {
+  /// Decrypt [keyStoreJson] to get topl's extended private key
+  Uint8List decryptKeyStore(String keyStoreJson, String password) {
+    const Base58Encoder base58Encoder = Base58Encoder.instance;
+    final KeyStore keyStore = KeyStore.fromV1Json(keyStoreJson, password);
+    final Uint8List toplExtendedPrvKeyUint8List = base58Encoder.decode(keyStore.privateKey);
+    return toplExtendedPrvKeyUint8List;
+  }
+}
