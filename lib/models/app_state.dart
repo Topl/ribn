a292ddfdb812ee595391f1b5c54f9b2f987f7937
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-07 15:20:44 -0500

    Add keychain state

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,17 +1,29 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+
+import 'package:ribn/models/keychain_state.dart';
+import 'package:ribn/models/login_state.dart';
 import 'package:ribn/models/onboarding_state.dart';
 
 @immutable
 class AppState {
   final OnboardingState onboardingState;
+  final LoginState loginState;
+  final KeyChainState keyChainState;
+
   const AppState({
     required this.onboardingState,
+    required this.loginState,
+    required this.keyChainState,
   });
 
   factory AppState.initial() {
-    return AppState(onboardingState: OnboardingState.initial());
+    return AppState(
+      onboardingState: OnboardingState.initial(),
+      loginState: LoginState.initial(),
+      keyChainState: KeyChainState.initial(),
+    );
   }
 
   bool keyStoreExists() {
@@ -22,29 +34,41 @@ class AppState {
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is AppState && other.onboardingState == onboardingState;
+    return other is AppState &&
+        other.onboardingState == onboardingState &&
+        other.loginState == loginState &&
+        other.keyChainState == keyChainState;
   }
 
   @override
-  int get hashCode => onboardingState.hashCode;
+  int get hashCode => onboardingState.hashCode ^ loginState.hashCode ^ keyChainState.hashCode;
 
   AppState copyWith({
     OnboardingState? onboardingState,
+    LoginState? loginState,
+    KeyChainState? keyChainState,
   }) {
     return AppState(
       onboardingState: onboardingState ?? this.onboardingState,
+      loginState: loginState ?? this.loginState,
+      keyChainState: keyChainState ?? this.keyChainState,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'onboardingState': onboardingState.toMap(),
+      'loginState': loginState.toMap(),
     };
   }
 
   factory AppState.fromMap(Map<String, dynamic> map) {
     return AppState(
       onboardingState: OnboardingState.fromMap(map['onboardingState']),
+      loginState: LoginState.fromMap(map['loginState']),
+      keyChainState: map.containsKey("keyChainState")
+          ? KeyChainState.fromMap(map['keyChainState'])
+          : KeyChainState.initial(),
     );
   }
 
@@ -53,5 +77,6 @@ class AppState {
   factory AppState.fromJson(String source) => AppState.fromMap(json.decode(source));
 
   @override
-  String toString() => 'AppState(onboardingState: $onboardingState)';
+  String toString() =>
+      'AppState(onboardingState: $onboardingState, loginState: $loginState, keyChainState: $keyChainState)';
 }
---
+++ lib/models/keychain_state.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:bip_topl/bip_topl.dart';
+
+class KeyChainState {
+  final String? keyStoreJson;
+  final Cip1852KeyTree? cip1852keyTree;
+
+  KeyChainState({
+    this.keyStoreJson,
+    this.cip1852keyTree,
+  });
+
+  factory KeyChainState.initial() {
+    return KeyChainState();
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'keyStoreJson': keyStoreJson,
+    };
+  }
+
+  factory KeyChainState.fromMap(Map<String, dynamic> map) {
+    return KeyChainState(
+      keyStoreJson: map['keyStoreJson'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory KeyChainState.fromJson(String source) => KeyChainState.fromMap(json.decode(source));
+
+  @override
+  String toString() => 'KeyChainState(keyStoreJson: $keyStoreJson, cip1852keyTree: $cip1852keyTree)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is KeyChainState &&
+        other.keyStoreJson == keyStoreJson &&
+        other.cip1852keyTree == cip1852keyTree;
+  }
+
+  @override
+  int get hashCode => keyStoreJson.hashCode ^ cip1852keyTree.hashCode;
+
+  KeyChainState copyWith({
+    String? keyStoreJson,
+    Cip1852KeyTree? cip1852keyTree,
+  }) {
+    return KeyChainState(
+      keyStoreJson: keyStoreJson ?? this.keyStoreJson,
+      cip1852keyTree: cip1852keyTree ?? this.cip1852keyTree,
+    );
+  }
+}
