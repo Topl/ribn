3b2290fc773ae900b281b016c59ff9650b9f89f0
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-08 10:35:20 -0500

    Add keychain reducer for updating KeyChainState

--- lib/reducers/keychain_reducer.dart
+++ lib/reducers/keychain_reducer.dart
@@ -1,18 +1,34 @@
 import 'package:bip_topl/bip_topl.dart';
 import 'package:redux/redux.dart';
 import 'package:ribn/actions/login_actions.dart';
+import 'package:ribn/actions/onboarding_actions.dart';
 import 'package:ribn/constants/rules.dart';
 import 'package:ribn/models/keychain_state.dart';
 
+/// Reducer responsible for updating [KeyChainState]
 final keyChainReducer = combineReducers<KeyChainState>(
   [
     TypedReducer<KeyChainState, LoginSuccessAction>(_onLoginSuccess),
+    TypedReducer<KeyChainState, PasswordSuccessfullyCreatedAction>(_onPasswordSuccessfullyCreated),
   ],
 );
 
-/// Create new Cip1852KeyTree upon successful login, where root = toplExtendedPrivateKey
+/// Creates new [Cip1852KeyTree] upon successful login, where root = toplExtendedPrivateKey
 KeyChainState _onLoginSuccess(KeyChainState keyChainState, LoginSuccessAction action) {
   return keyChainState.copyWith(
+    cip1852keyTree: Cip1852KeyTree()
+      ..root = Bip32SigningKey.fromValidBytes(
+        action.toplExtendedPrivateKey,
+        depth: Rules.toplKeyDepth,
+      ),
+  );
+}
+
+/// Creates the keyStoreJson and cip1852KeyTree upon successfull password creation
+KeyChainState _onPasswordSuccessfullyCreated(
+    KeyChainState keyChainState, PasswordSuccessfullyCreatedAction action) {
+  return keyChainState.copyWith(
+    keyStoreJson: action.keyStoreJson,
     cip1852keyTree: Cip1852KeyTree()
       ..root = Bip32SigningKey.fromValidBytes(
         action.toplExtendedPrivateKey,
