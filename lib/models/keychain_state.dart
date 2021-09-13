972f39b1c9f4879cb24688d489863454bccde0ee
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-13 13:07:29 -0500

    Update keychain state

--- lib/models/keychain_state.dart
+++ lib/models/keychain_state.dart
@@ -1,29 +1,40 @@
+// ignore_for_file: implementation_imports
 import 'dart:convert';
-
-import 'package:bip_topl/bip_topl.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mubrambl/src/credentials/hd_wallet_helper.dart';
+import 'package:ribn/constants/rules.dart';
+import 'package:ribn/models/ribn_address.dart';
 
 class KeyChainState {
   final String? keyStoreJson;
-  final Cip1852KeyTree? cip1852keyTree; // never persisted
+  final HdWallet? hdWallet; // never persisted
+  final List<RibnAddress> addresses;
 
   KeyChainState({
     this.keyStoreJson,
-    this.cip1852keyTree,
+    this.hdWallet,
+    this.addresses = const [],
   });
 
   factory KeyChainState.initial() {
     return KeyChainState();
   }
 
+  int getNextExternalAddressIndex() {
+    return addresses.lastWhere((addr) => addr.changeIndex == Rules.defaultChangeIndex).addressIndex + 1;
+  }
+
   Map<String, dynamic> toMap() {
     return {
       'keyStoreJson': keyStoreJson,
+      'addresses': addresses.map((x) => x.toMap()).toList(),
     };
   }
 
   factory KeyChainState.fromMap(Map<String, dynamic> map) {
     return KeyChainState(
       keyStoreJson: map['keyStoreJson'],
+      addresses: List<RibnAddress>.from(map['addresses']?.map((x) => RibnAddress.fromMap(x))),
     );
   }
 
@@ -32,7 +43,8 @@ class KeyChainState {
   factory KeyChainState.fromJson(String source) => KeyChainState.fromMap(json.decode(source));
 
   @override
-  String toString() => 'KeyChainState(keyStoreJson: $keyStoreJson, cip1852keyTree: $cip1852keyTree)';
+  String toString() =>
+      'KeyChainState(keyStoreJson: $keyStoreJson, hdWallet: $hdWallet, addresses: $addresses)';
 
   @override
   bool operator ==(Object other) {
@@ -40,19 +52,22 @@ class KeyChainState {
 
     return other is KeyChainState &&
         other.keyStoreJson == keyStoreJson &&
-        other.cip1852keyTree == cip1852keyTree;
+        other.hdWallet == hdWallet &&
+        listEquals(other.addresses, addresses);
   }
 
   @override
-  int get hashCode => keyStoreJson.hashCode ^ cip1852keyTree.hashCode;
+  int get hashCode => keyStoreJson.hashCode ^ hdWallet.hashCode ^ addresses.hashCode;
 
   KeyChainState copyWith({
     String? keyStoreJson,
-    Cip1852KeyTree? cip1852keyTree,
+    HdWallet? hdWallet,
+    List<RibnAddress>? addresses,
   }) {
     return KeyChainState(
       keyStoreJson: keyStoreJson ?? this.keyStoreJson,
-      cip1852keyTree: cip1852keyTree ?? this.cip1852keyTree,
+      hdWallet: hdWallet ?? this.hdWallet,
+      addresses: addresses ?? this.addresses,
     );
   }
 }
