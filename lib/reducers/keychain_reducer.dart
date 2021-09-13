0087cbf25cd56038e4584316bb5d30e1025d536b
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-13 13:09:36 -0500

    Update to use hdwallet helper

--- lib/reducers/keychain_reducer.dart
+++ lib/reducers/keychain_reducer.dart
@@ -1,38 +1,38 @@
+// ignore_for_file: implementation_imports
+
 import 'package:bip_topl/bip_topl.dart';
+import 'package:mubrambl/src/credentials/hd_wallet_helper.dart';
 import 'package:redux/redux.dart';
-import 'package:ribn/actions/login_actions.dart';
-import 'package:ribn/actions/onboarding_actions.dart';
+import 'package:ribn/actions/keychain_actions.dart';
 import 'package:ribn/constants/rules.dart';
 import 'package:ribn/models/keychain_state.dart';
 
 /// Reducer responsible for updating [KeyChainState]
 final keyChainReducer = combineReducers<KeyChainState>(
   [
-    TypedReducer<KeyChainState, LoginSuccessAction>(_onLoginSuccess),
-    TypedReducer<KeyChainState, PasswordSuccessfullyCreatedAction>(_onPasswordSuccessfullyCreated),
+    TypedReducer<KeyChainState, InitializeHDWalletAction>(_onHdWalletInitialization),
+    TypedReducer<KeyChainState, AddAddressesAction>(_onAddAddresses),
   ],
 );
 
-/// Creates new [Cip1852KeyTree] upon successful login, where root = toplExtendedPrivateKey
-KeyChainState _onLoginSuccess(KeyChainState keyChainState, LoginSuccessAction action) {
+/// Creates a new [HdWallet] upon successful login, where root = toplExtendedPrivateKey.
+///
+/// Optionally updates the [keyStoreJson], for instance, when creating a new wallet for the first time.
+KeyChainState _onHdWalletInitialization(KeyChainState keyChainState, InitializeHDWalletAction action) {
   return keyChainState.copyWith(
-    cip1852keyTree: Cip1852KeyTree()
-      ..root = Bip32SigningKey.fromValidBytes(
+    keyStoreJson: action.keyStoreJson ?? keyChainState.keyStoreJson,
+    hdWallet: HdWallet(
+      rootSigningKey: Bip32SigningKey.fromValidBytes(
         action.toplExtendedPrivateKey,
         depth: Rules.toplKeyDepth,
       ),
+    ),
   );
 }
 
-/// Creates the keyStoreJson and cip1852KeyTree upon successfull password creation
-KeyChainState _onPasswordSuccessfullyCreated(
-    KeyChainState keyChainState, PasswordSuccessfullyCreatedAction action) {
+/// Updates the list of addresses in [KeyChainState].
+KeyChainState _onAddAddresses(KeyChainState keyChainState, AddAddressesAction action) {
   return keyChainState.copyWith(
-    keyStoreJson: action.keyStoreJson,
-    cip1852keyTree: Cip1852KeyTree()
-      ..root = Bip32SigningKey.fromValidBytes(
-        action.toplExtendedPrivateKey,
-        depth: Rules.toplKeyDepth,
-      ),
+    addresses: keyChainState.addresses.toList()..addAll(action.addresses),
   );
 }
--- lib/repositories/onboarding_repository.dart
+++ lib/repositories/onboarding_repository.dart
@@ -4,14 +4,10 @@ import 'dart:math';
 import 'dart:typed_data';
 import 'package:bip_topl/bip_topl.dart';
 import 'package:mubrambl/src/crypto/keystore.dart';
+import 'package:mubrambl/src/credentials/hd_wallet_helper.dart';
 import 'package:ribn/constants/rules.dart';
 
 class OnboardingRespository {
-  final Cip1852KeyTree cip1852keyTree;
-  OnboardingRespository({
-    required this.cip1852keyTree,
-  });
-
   /// Generates the mnemonic to be shown to the user.
   ///
   /// Derives the Topl extended private key that follows the path described in [Rules.toplKeyPath].
@@ -21,20 +17,37 @@ class OnboardingRespository {
     final Random random = Random.secure();
     const Base58Encoder base58Encoder = Base58Encoder.instance;
     final String mnemonic = generateMnemonic(random);
-    final Bip32Key rootBip32Key = cip1852keyTree.master(mnemonicToSeed(mnemonic));
-    cip1852keyTree.root = rootBip32Key;
-    final Bip32Key toplExtendedPrvKey = cip1852keyTree.pathToKey(Rules.toplKeyPath);
-    Uint8List toplExtendedPrvKeyUint8List = Uint8List.fromList(
-      [...toplExtendedPrvKey.keyBytes, ...toplExtendedPrvKey.chainCode],
-    );
+    final Bip32KeyPair toplExtendedKeyPair = deriveToplExtendedKeys(mnemonic);
+    Uint8List toplExtendedPrvKeyUint8List = Uint8List.fromList(toplExtendedKeyPair.privateKey!);
     final String base58EncodedToplExtendedPrvKey = base58Encoder.encode(toplExtendedPrvKeyUint8List);
-    final KeyStore keyStore =
-        KeyStore.createNew(base58EncodedToplExtendedPrvKey, password, random, scryptN: 4);
+    final KeyStore keyStore = KeyStore.createNew(
+      base58EncodedToplExtendedPrvKey,
+      password,
+      random,
+      scryptN: 4,
+    );
     final String keyStoreJson = keyStore.toJson();
     return {
       'mnemonic': mnemonic,
       'keyStoreJson': keyStoreJson,
       'toplExtendedPrvKeyUint8List': toplExtendedPrvKeyUint8List,
     };
+  }
+
+  Bip32KeyPair deriveToplExtendedKeys(String mnemonic) {
+    final HdWallet hdWallet = HdWallet.fromMnemonic(mnemonic);
+    final Bip32KeyPair rootKeys = Bip32KeyPair(
+      privateKey: hdWallet.rootSigningKey,
+      publicKey: hdWallet.rootVerifyKey,
+    );
+    final Bip32KeyPair layerOneKeyPair = hdWallet.derive(
+      keys: rootKeys,
+      index: Rules.defaultPurpose,
+    );
+    final Bip32KeyPair toplExtendedKeyPair = hdWallet.derive(
+      keys: layerOneKeyPair,
+      index: Rules.defaultCoinType,
+    );
+    return toplExtendedKeyPair;
   }
 }
