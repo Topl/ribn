ad551cacb5eb76c028a5d7611b3e3934ba8fb47c
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-24 16:17:02 -0500

    Update constants

--- lib/constants/rules.dart
+++ lib/constants/rules.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: implementation_imports
 
 import 'package:mubrambl/src/credentials/hd_wallet_helper.dart' as hd;
+import 'package:mubrambl/src/utils/constants.dart' as constants;
+import 'package:mubrambl/src/utils/network.dart';
+
 import 'package:ribn/constants/strings.dart';
-import 'package:ribn/models/ribn_address.dart';
 import 'package:mubrambl/src/core/client.dart';
 import 'package:mubrambl/src/auth/auth.dart';
 import 'package:dio/dio.dart';
@@ -10,30 +12,31 @@ import 'package:dio/dio.dart';
 class Rules {
   static const int minPasswordLength = 8;
   static const int scryptN = 8192;
-  static const int extendedSecretKeySize = 64;
+  static const int extendedSecretKeySize = hd.extended_secret_key_size;
   static const int toplKeyDepth = 2;
-  static const int hardenedOffset = 0x80000000;
+  static const int hardenedOffset = constants.HARDENED_OFFSET;
   // Reference: [CIP-1852](https://github.com/cardano-foundation/CIPs/blob/master/CIP-1852/CIP-1852.md)
   // m / purpose' / coin_type' / account' / role / index
-  static const int defaultPurpose = 1852 | hardenedOffset; // 1852'
-  static const int defaultCoinType = 7091 | hardenedOffset; // 7091'
-  static const int defaultAccountIndex = 0 | hardenedOffset; // 0'
-  static const int defaultChangeIndex = 0; // 0=external/payments, 1=internal/change, 2=staking
-  static const int defaultAddressIndex = 0;
+  static const int defaultPurpose = constants.DEFAULT_PURPOSE; // 1852'
+  static const int defaultCoinType = constants.DEFAULT_COIN_TYPE; // 7091'
+  static const int defaultAccountIndex = constants.DEFAULT_ACCOUNT_INDEX; // 0'
+  static const int defaultChangeIndex =
+      constants.DEFAULT_CHANGE; // 0=external/payments, 1=internal/change, 2=staking
+  static const int defaultAddressIndex = constants.DEFAULT_ADDRESS_INDEX;
   static const int numInitialAddresses = 5;
   static const List<String> settings = [Strings.logout];
   static const int numHomeTabs = 3;
-  static const int toplnetId = 0x01;
-  static const int valhallaId = 0x10;
-  static const int privateId = 0x40;
-  static const Map<int, String> networkStrings = {
+  static int toplnetId = NETWORK_REGISTRY[Strings.toplnet]!;
+  static int valhallaId = NETWORK_REGISTRY[Strings.valhalla]!;
+  static int privateId = NETWORK_REGISTRY[Strings.private]!;
+  static Map<int, String> networkStrings = {
     valhallaId: Strings.valhalla,
     toplnetId: Strings.toplnet,
     privateId: Strings.private
   };
   static const String baasApiKey = "Mjc0ODg3MTktYTU3ZS00MGM2LWJkMmMtYTRjMzQxMWY3MjM4";
   static const String projectId = "60ff001754b7c75558146daf";
-  static const Map<int, String> networkUrls = {
+  static Map<int, String> networkUrls = {
     valhallaId: "https://staging.vertx.topl.services/valhalla/$projectId",
     toplnetId: "https://staging.vertx.topl.services/valhalla/$projectId", // temporary url
     privateId: "http://localhost:9085"
