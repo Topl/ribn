12f31e91fecf1320b7687e28a0b3f652227c87c0
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-27 09:33:29 -0500

    Remove unused import

--- lib/presentation/home_page.dart
+++ lib/presentation/home_page.dart
@@ -10,7 +10,6 @@ import 'package:ribn/constants/ui_constants.dart';
 import 'package:ribn/containers/home_container.dart';
 import 'package:ribn/presentation/address_section.dart';
 import 'package:flutter/services.dart';
-import 'package:ribn/presentation/transaction_section.dart';
 import 'package:mubrambl/src/core/amount.dart';
 
 class HomePage extends StatefulWidget {
