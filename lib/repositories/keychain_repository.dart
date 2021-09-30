d1984a90cabad4616105c2d5bb60524ab97dcb10
Mustafa Ali <mustafaali.py@gmail.com>
2021-09-30 09:57:45 -0500

    Update keychain repo

--- lib/repositories/keychain_repository.dart
+++ lib/repositories/keychain_repository.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: implementation_imports
+import 'dart:typed_data';
+
+import 'package:bip_topl/bip_topl.dart';
 import 'package:ribn/constants/rules.dart';
 import 'package:ribn/models/ribn_address.dart';
 import 'package:mubrambl/src/credentials/hd_wallet_helper.dart';
 import 'package:mubrambl/src/credentials/address.dart';
+import 'package:mubrambl/src/credentials/credentials.dart';
 import 'package:mubrambl/src/core/client.dart';
 import 'package:mubrambl/src/model/balances.dart';
+import 'package:mubrambl/src/utils/proposition_type.dart';
 
 class KeychainRepository {
   const KeychainRepository();
@@ -17,12 +22,13 @@ class KeychainRepository {
     int addr = Rules.defaultAddressIndex,
     required int networkId,
   }) {
-    Bip32KeyPair keyPair = hdWallet.deriveLastThreeLayers(address: addr);
+    Bip32KeyPair keyPair = hdWallet.deriveLastThreeLayers(account: account, change: change, address: addr);
     ToplAddress toplAddress = hdWallet.toBaseAddress(spend: keyPair.publicKey!, networkId: networkId);
     String keyPath = getKeyPath(purpose, coinType, account, change, addr);
     RibnAddress newAddress = RibnAddress(
       address: toplAddress,
       addressIndex: addr,
+      changeIndex: change,
       keyPath: keyPath,
       balance: Rules.initBalance(toplAddress.toBase58()),
     );
@@ -44,5 +50,22 @@ class KeychainRepository {
     List<ToplAddress> addresses,
   ) async {
     return await client.getAllAddressBalances(addresses);
+  }
+
+  Credentials getCredentials(
+    HdWallet hdWallet, {
+    int account = Rules.defaultAccountIndex,
+    int change = Rules.defaultChangeIndex,
+    int addr = Rules.defaultAddressIndex,
+    required int networkId,
+    PropositionType? propositionType,
+  }) {
+    Bip32KeyPair keyPair = hdWallet.deriveLastThreeLayers(account: account, change: change, address: addr);
+    String base58EncodedPrivKey = Base58Encoder.instance.encode(Uint8List.fromList(keyPair.privateKey!));
+    return ToplSigningKey.fromString(
+      base58EncodedPrivKey,
+      networkId,
+      propositionType ?? PropositionType.ed25519(),
+    );
   }
 }
