76d272edbec41499682b3a8834a754d70034b362
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-13 14:36:51 -0500

    Update misc actions

--- lib/actions/misc_actions.dart
+++ lib/actions/misc_actions.dart
@@ -12,3 +12,13 @@ class NavigateToRoute {
   final Object? arguments;
   NavigateToRoute(this.route, {this.arguments});
 }
+
+class SendInternalMsgAction {
+  final String msg;
+  SendInternalMsgAction(this.msg);
+}
+
+class ReceiveInternalMsgAction {
+  final String msg;
+  ReceiveInternalMsgAction(this.msg);
+}
