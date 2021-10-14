a5cef9ad747b34325012a8a64083d07e06203ab1
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-14 16:08:35 -0500

    Add enable page

---
+++ lib/presentation/enable_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:ribn/actions/misc_actions.dart';
+import 'package:ribn/constants/strings.dart';
+import 'package:ribn/constants/ui_constants.dart';
+import 'package:ribn/models/app_state.dart';
+import 'package:ribn/models/internal_message.dart';
+
+class EnablePage extends StatelessWidget {
+  final InternalMessage pendingRequest;
+  const EnablePage(this.pendingRequest, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Grant permission to: ${pendingRequest.origin}',
+          style: const TextStyle(
+            fontSize: UIConstants.smallTextSize,
+          ),
+        ),
+        MaterialButton(
+          child: const Text('Accept'),
+          onPressed: () => sendResponse(context, true),
+        ),
+        MaterialButton(
+          child: const Text('Reject'),
+          onPressed: () => sendResponse(context, false),
+        ),
+      ],
+    );
+  }
+
+  void sendResponse(BuildContext context, bool accept) {
+    final InternalMessage response = pendingRequest.copyWith(
+      method: Strings.returnResponse,
+      sender: InternalMessage.defaultSender,
+      data: {
+        'enable': accept,
+      },
+    );
+    StoreProvider.of<AppState>(context).dispatch(SendInternalMsgAction(response));
+  }
+}
