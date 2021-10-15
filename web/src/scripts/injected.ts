4027a42013350f550a97bd977ecc4e2ea7761df0
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-15 13:29:12 -0500

    Cleanup

--- web/src/scripts/injected.ts
+++ web/src/scripts/injected.ts
@@ -1,6 +1,5 @@
 import { enable, signTx, isEnabled } from "../api/webpage";
 
-// Reference: https://github.com/cardano-foundation/CIPs/pull/88
 window["topl"] = {
 	enable: () => enable(),
 	isEnabled: () => isEnabled(),
