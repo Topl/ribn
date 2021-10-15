fb13504f19074e776c727b6c52e31fda31d3eb0c
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-15 13:35:45 -0500

    Update tx middleware

--- lib/middlewares/transaction_middleware.dart
+++ lib/middlewares/transaction_middleware.dart
@@ -1,7 +1,4 @@
-// ignore_for_file: implementation_imports
-
-import 'package:mubrambl/brambldart.dart';
-import 'package:mubrambl/src/credentials/credentials.dart';
+import 'package:brambldart/brambldart.dart';
 import 'package:redux/redux.dart';
 import 'package:ribn/actions/keychain_actions.dart';
 import 'package:ribn/actions/misc_actions.dart';
@@ -9,6 +6,7 @@ import 'package:ribn/actions/transaction_actions.dart';
 import 'package:ribn/constants/rules.dart';
 import 'package:ribn/constants/strings.dart';
 import 'package:ribn/models/app_state.dart';
+import 'package:ribn/models/internal_message.dart';
 import 'package:ribn/models/ribn_address.dart';
 import 'package:ribn/models/ribn_network.dart';
 import 'package:ribn/repositories/keychain_repository.dart';
@@ -24,6 +22,7 @@ List<Middleware<AppState>> createTransactionMiddleware(
     TypedMiddleware<AppState, CreateRawTxAction>(_createRawTx(transactionRepo)),
     TypedMiddleware<AppState, SignTxAction>(_signTx(transactionRepo, keychainRepo)),
     TypedMiddleware<AppState, BroadcastTxAction>(_broadcastTx(transactionRepo)),
+    TypedMiddleware<AppState, SignExternalTxAction>(_signExternalTx(transactionRepo, keychainRepo)),
   ];
 }
 
@@ -32,7 +31,7 @@ void Function(Store<AppState> store, InitiateTxAction action, NextDispatcher nex
   return (store, action, next) async {
     try {
       RibnNetwork currNetwork = store.state.keychainState.currentNetwork;
-      RibnAddress senderAddr = transactionRepo.getSenderAddress(
+      List<RibnAddress> senders = transactionRepo.getSenderAddresses(
         action.transferDetails[Strings.transferType] as String,
         currNetwork,
         polyAmount: int.parse(action.transferDetails[Strings.amount] as String),
@@ -47,7 +46,7 @@ void Function(Store<AppState> store, InitiateTxAction action, NextDispatcher nex
       Map<String, dynamic> transferDetails = {
         ...action.transferDetails,
         Strings.change: changeAddr,
-        Strings.sender: senderAddr,
+        Strings.sender: senders,
         Strings.networkId: currNetwork.networkId,
       };
       next(CreateRawTxAction(transferDetails));
@@ -80,17 +79,16 @@ void Function(Store<AppState> store, SignTxAction action, NextDispatcher next) _
     TransactionRepository transactionRepo, KeychainRepository keychainRepo) {
   return (store, action, next) async {
     try {
-      RibnAddress senderAddr = action.transferDetails[Strings.sender] as RibnAddress;
-      Credentials credentials = keychainRepo.getCredentials(
+      final List<RibnAddress> senders = action.transferDetails[Strings.sender] as List<RibnAddress>;
+      final List<Credentials> credentials = keychainRepo.getCredentials(
         store.state.keychainState.hdWallet!,
-        account: senderAddr.accountIndex,
-        change: senderAddr.changeIndex,
-        addr: senderAddr.addressIndex,
-        propositionType: senderAddr.address.proposition,
-        networkId: senderAddr.address.networkId,
+        senders,
       );
-      BramblClient client = store.state.keychainState.currentNetwork.client!;
-      TransactionReceipt signedTx = await transactionRepo.signTx(client, credentials, action.transferDetails);
+      final TransactionReceipt signedTx = await transactionRepo.signTx(
+        store.state.keychainState.currentNetwork.client!,
+        credentials,
+        action.transferDetails,
+      );
       next(
         BroadcastTxAction(
           signedTx,
@@ -111,12 +109,56 @@ void Function(Store<AppState> store, BroadcastTxAction action, NextDispatcher ne
         store.state.keychainState.currentNetwork.client!,
         action.signedTx,
       );
-      // add the newly generated change address to the wallet
       if (action.changeAddress != null) {
         store.dispatch(AddAddressesAction(addresses: [action.changeAddress!]));
       }
     } catch (e) {
       next(ApiErrorAction(e.toString()));
+    }
+  };
+}
+
+void Function(Store<AppState> store, SignExternalTxAction action, NextDispatcher next) _signExternalTx(
+    TransactionRepository transactionRepo, KeychainRepository keychainRepo) {
+  return (store, action, next) async {
+    try {
+      final Map<String, dynamic> transferDetails = action.pendingRequest.data!;
+      transferDetails['messageToSign'] =
+          Base58Data.validated(transferDetails['messageToSign'] as String).value;
+      final TransactionReceipt transactionReceipt = TransactionReceipt.fromJson(transferDetails);
+      transferDetails['rawTx'] = transactionReceipt;
+      final List<String> rawTxSenders =
+          transactionReceipt.from!.map((e) => e.senderAddress.toBase58()).toList();
+      final List<RibnAddress> sendersInWallet = List.from(store.state.keychainState.currentNetwork.addresses)
+        ..retainWhere((addr) => rawTxSenders.contains(addr.address.toBase58()));
+      final List<Credentials> credentials = keychainRepo.getCredentials(
+        store.state.keychainState.hdWallet!,
+        sendersInWallet,
+      );
+      final TransactionReceipt signedTx = await transactionRepo.signTx(
+        store.state.keychainState.currentNetwork.client!,
+        credentials,
+        transferDetails,
+      );
+      final InternalMessage response = InternalMessage(
+        method: Strings.returnResponse,
+        data: signedTx.toBroadcastJson(),
+        target: action.pendingRequest.target,
+        sender: 'ribn',
+        id: action.pendingRequest.id,
+        origin: action.pendingRequest.origin,
+      );
+      next(SendInternalMsgAction(response));
+    } catch (e) {
+      final InternalMessage response = InternalMessage(
+        method: Strings.returnResponse,
+        data: {'error': e.toString()},
+        target: action.pendingRequest.target,
+        sender: 'ribn',
+        id: action.pendingRequest.id,
+        origin: action.pendingRequest.origin,
+      );
+      next(SendInternalMsgAction(response));
     }
   };
 }
