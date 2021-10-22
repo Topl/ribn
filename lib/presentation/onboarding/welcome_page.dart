51dd3b71e0f3a4ffe3c333f85f825231447c4244
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-22 15:35:09 -0500

    Update welcome page

---
+++ lib/presentation/onboarding/welcome_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ribn/actions/misc_actions.dart';
+import 'package:ribn/constants/assets.dart';
+
+import 'package:ribn/constants/routes.dart';
+import 'package:ribn/constants/strings.dart';
+import 'package:ribn/constants/styles.dart';
+import 'package:ribn/models/app_state.dart';
+import 'package:ribn/widgets/continue_button.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              Strings.welcomeToRibn,
+              style: RibnTextStyles.headingOne,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 42),
+            SvgPicture.asset(
+              RibnAssets.logoIcon,
+              height: 128,
+              width: 132,
+            ),
+            const SizedBox(height: 23),
+            const SizedBox(
+              width: 575,
+              height: 83,
+              child: Center(
+                child: Text(
+                  Strings.intro,
+                  style: RibnTextStyles.bodyOne,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ContinueButton(
+              Strings.getStarted,
+              () => StoreProvider.of<AppState>(context).dispatch(NavigateToRoute(Routes.selectAction)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
