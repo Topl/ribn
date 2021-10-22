bffdeae10200840ec921c65e13b6ab8314fe9436
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-22 15:22:15 -0500

    Add assets

---
+++ lib/constants/assets.dart
@@ -0,0 +1,12 @@
+/// All the assets that are being used throughout the app
+class RibnAssets {
+  RibnAssets._();
+  static const logoIcon = 'assets/icons/topl_placeholder.svg';
+  static const logoCardIcon = 'assets/icons/topl_placeholder_card.svg';
+  static const warningIcon = 'assets/icons/warning.svg';
+  static const paperPenIcon = 'assets/icons/paper_pen.svg';
+  static const seedPhraseConfirmedIcon = 'assets/icons/seed_phrase_confirmed.svg';
+  static const walletPasswordIcon = 'assets/icons/wallet_password.svg';
+  static const addIcon = 'assets/icons/add.svg';
+  static const removeIcon = 'assets/icons/remove.svg';
+}
