f33a0f215667daa5911c89924a912b31846036d4
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-26 16:37:38 -0500

    Update routes

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,13 +1,17 @@
 class Routes {
-  static const welcome = "/welcome";
-  static const selectAction = "/select-action";
-  static const createPassword = "/create-password";
-  static const seedPhrase = "/seed-phrase";
-  static const seedPhraseConfirm = "/seed-phrase-confirm";
-  static const login = "/login";
-  static const home = "/home";
-  static const addresses = "/addresses";
-  static const externalSigning = "/ext-signing";
-  static const enable = "/enable";
-  static const error = "/error";
+  static const welcome = '/welcome';
+  static const selectAction = '/select-action';
+  static const gettingStarted = '/getting-started';
+  static const readCarefully = '/read-carefully';
+  static const createPassword = '/create-password';
+  static const onboardingSteps = '/onboarding-steps';
+  static const seedPhrase = '/seed-phrase';
+  static const seedPhraseConfirm = '/seed-phrase-confirm';
+  static const extensionInfo = '/extension-info';
+  static const login = '/login';
+  static const home = '/home';
+  static const addresses = '/addresses';
+  static const externalSigning = '/ext-signing';
+  static const enable = '/enable';
+  static const error = '/error';
 }
