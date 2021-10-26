fb908565e990069b5fb07e4467b5c9a8737f4507
Mustafa Ali <mustafaali.py@gmail.com>
2021-10-26 09:39:15 -0500

    Update colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RibnColors {
+  RibnColors._();
+  static const Color primary = Color(0xFF26a69a);
+  static const Color accent = Color(0xFFD2F6F2);
+  static const Color defaultText = Color(0xFF323232);
+  static const Color inactive = Color(0xFFDADADA);
+  static const Color active = Color(0xFF2AD062);
+}
