dba019e991b4c04eb0cfe5d37feabb6b31d4bbe5
Mustafa Ali <mustafaali@Mustafas-MacBook-Pro-2.local>
2021-12-07 19:19:07 +0500

    Update walletbalance container

--- lib/containers/wallet_balance_container.dart
+++ lib/containers/wallet_balance_container.dart
@@ -1,17 +1,13 @@
-// ignore_for_file: unused_import
-
 import 'package:brambldart/model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:redux/redux.dart';
 import 'package:ribn/actions/keychain_actions.dart';
 import 'package:ribn/actions/misc_actions.dart';
-import 'package:ribn/constants/assets.dart';
 import 'package:ribn/constants/routes.dart';
-import 'package:ribn/constants/strings.dart';
 import 'package:ribn/models/app_state.dart';
 
+/// Intended to wrap the [WalletBalancePage] and provide it with the the [WalletBalanceViewModel].
 class WalletBalanceContainer extends StatelessWidget {
   const WalletBalanceContainer({Key? key, required this.builder}) : super(key: key);
   final ViewModelBuilder<WalletBalanceViewModel> builder;
@@ -31,31 +27,33 @@ class WalletBalanceViewModel {
   final int currentNetwork;
   final Function(String) updateNetwork;
   final List<AssetAmount> assets;
+  final Function(AssetAmount) initiateSendAsset;
 
   WalletBalanceViewModel({
     required this.polyBalance,
     required this.updateNetwork,
     required this.currentNetwork,
     required this.assets,
+    required this.initiateSendAsset,
   });
   static WalletBalanceViewModel fromStore(Store<AppState> store) {
     return WalletBalanceViewModel(
-      /// @TODO
-      polyBalance: 100,
-      // store.state.keychainState.currentNetwork.addresses.fold(
-      //   0,
-      //   (prev, currBalance) => prev + currBalance.balance.polys.getInNanopoly,
-      // ),
+      polyBalance: store.state.keychainState.currentNetwork.addresses.fold(
+        0,
+        (prev, currBalance) => prev + currBalance.balance.polys.getInNanopoly,
+      ),
       currentNetwork: store.state.keychainState.currentNetwork.networkId,
       updateNetwork: (String networkId) {
         store.dispatch(UpdateCurrentNetworkAction(networkId));
         store.dispatch(RefreshBalancesAction());
       },
-      assets: [],
-      // store.state.keychainState.currentNetwork.addresses
-      //     .map((addr) => addr.balance.assets ?? [])
-      //     .expand((amount) => amount)
-      //     .toList(),
+      assets: store.state.keychainState.currentNetwork.addresses
+          .map((addr) => addr.balance.assets ?? [])
+          .expand((amount) => amount)
+          .toList(),
+      initiateSendAsset: (AssetAmount asset) => store.dispatch(
+        NavigateToRoute(Routes.assetTransferInput, arguments: asset),
+      ),
     );
   }
 }
