5be518c0b23ed9ebe12aea08afa061540a4b4ce2
Mustafa Ali <mustafaali@Mustafas-MacBook-Pro-2.local>
2021-12-07 16:28:19 +0500

    Update containers

---
+++ lib/containers/asset_transfer_input_container.dart
@@ -0,0 +1,69 @@
+import 'package:brambldart/brambldart.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import 'package:ribn/actions/transaction_actions.dart';
+import 'package:ribn/constants/strings.dart';
+import 'package:ribn/models/app_state.dart';
+import 'package:ribn/models/transfer_details.dart';
+
+/// Intended to wrap the [AssetTransferInputPage] and provide it with the the [AssetTransferInputViewModel].
+class AssetTransferInputContainer extends StatelessWidget {
+  const AssetTransferInputContainer({Key? key, required this.builder}) : super(key: key);
+  final ViewModelBuilder<AssetTransferInputViewModel> builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, AssetTransferInputViewModel>(
+      distinct: true,
+      converter: AssetTransferInputViewModel.fromStore,
+      builder: builder,
+    );
+  }
+}
+
+class AssetTransferInputViewModel {
+  final List<AssetAmount> assets;
+  final Function(String, String, String, AssetCode) initiateTx;
+  final bool loadingRawTx;
+  AssetTransferInputViewModel({
+    required this.assets,
+    required this.initiateTx,
+    required this.loadingRawTx,
+  });
+
+  static AssetTransferInputViewModel fromStore(Store<AppState> store) {
+    return AssetTransferInputViewModel(
+      initiateTx: (String recipient, String amount, String note, AssetCode assetCode) {
+        final TransferDetails transferDetails = TransferDetails(
+          transferType: Strings.assetTransfer,
+          recipient: recipient,
+          amount: amount,
+          data: note,
+          assetCode: assetCode,
+        );
+        store.dispatch((InitiateTxAction(transferDetails)));
+      },
+      assets: store.state.keychainState.currentNetwork.addresses
+          .map((addr) => addr.balance.assets ?? [])
+          .expand((amount) => amount)
+          .toList(),
+      loadingRawTx: store.state.uiState.loadingRawTx,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is AssetTransferInputViewModel &&
+        listEquals(other.assets, assets) &&
+        other.initiateTx == initiateTx &&
+        other.loadingRawTx == loadingRawTx;
+  }
+
+  @override
+  int get hashCode => assets.hashCode ^ initiateTx.hashCode ^ loadingRawTx.hashCode;
+}
---
+++ lib/containers/mint_input_container.dart
@@ -0,0 +1,67 @@
+import 'package:brambldart/brambldart.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import 'package:ribn/actions/transaction_actions.dart';
+import 'package:ribn/constants/rules.dart';
+import 'package:ribn/constants/strings.dart';
+import 'package:ribn/models/app_state.dart';
+import 'package:ribn/models/transfer_details.dart';
+
+/// Intended to wrap the [MintInputPage] and provide it with the the [MintInputViewmodel].
+class MintInputContainer extends StatelessWidget {
+  const MintInputContainer({Key? key, required this.builder}) : super(key: key);
+  final ViewModelBuilder<MintInputViewmodel> builder;
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<AppState, MintInputViewmodel>(
+      distinct: true,
+      converter: MintInputViewmodel.fromStore,
+      builder: builder,
+    );
+  }
+}
+
+class MintInputViewmodel {
+  final void Function(String, String, String) initiateTx;
+  final bool loadingRawTx;
+  MintInputViewmodel({
+    required this.initiateTx,
+    required this.loadingRawTx,
+  });
+
+  static MintInputViewmodel fromStore(Store<AppState> store) {
+    return MintInputViewmodel(
+      initiateTx: (String assetShortName, String amount, String note) {
+        final ToplAddress issuerAddress = store.state.keychainState.currentNetwork.addresses.first.address;
+        final TransferDetails transferDetails = TransferDetails(
+          transferType: Strings.minting,
+          assetCode: AssetCode.initialize(
+            Rules.assetCodeVersion,
+            issuerAddress,
+            assetShortName,
+            Rules.networkStrings[store.state.keychainState.currentNetwork.networkId]!,
+          ),
+          recipient: issuerAddress.toBase58(),
+          amount: amount,
+          data: note,
+        );
+        store.dispatch(InitiateTxAction(transferDetails));
+      },
+      loadingRawTx: store.state.uiState.loadingRawTx,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is MintInputViewmodel && other.initiateTx == initiateTx && other.loadingRawTx == loadingRawTx;
+  }
+
+  @override
+  int get hashCode => initiateTx.hashCode ^ loadingRawTx.hashCode;
+}
