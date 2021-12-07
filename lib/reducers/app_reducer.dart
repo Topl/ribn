4b12c1794ee1d9b74cf692d5d9e75636d42d6244
Mustafa Ali <mustafaali@Mustafas-MacBook-Pro-2.local>
2021-12-07 17:33:08 +0500

    Update reduceer

--- lib/reducers/app_reducer.dart
+++ lib/reducers/app_reducer.dart
@@ -2,6 +2,7 @@ import 'package:ribn/models/app_state.dart';
 import 'package:ribn/reducers/keychain_reducer.dart';
 import 'package:ribn/reducers/login_reducer.dart';
 import 'package:ribn/reducers/onboarding_reducer.dart';
+import 'package:ribn/reducers/ui_reducer.dart';
 
 /// Main reducer for [AppState]
 AppState appReducer(AppState state, dynamic action) {
@@ -9,5 +10,6 @@ AppState appReducer(AppState state, dynamic action) {
     onboardingState: onboardingReducer(state.onboardingState, action),
     loginState: loginReducer(state.loginState, action),
     keychainState: keychainReducer(state.keychainState, action),
+    uiState: uiReducer(state.uiState, action),
   );
 }
