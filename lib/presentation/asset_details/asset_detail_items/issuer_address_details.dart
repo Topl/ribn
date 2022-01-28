37033cb746044c67f4da2d9fed24808c685a0b86
Mustafa Ali <mustafaali@toplmbp.lan>
2022-01-28 16:03:05 -0600

    Add asset details page

---
+++ lib/presentation/asset_details/asset_detail_items/issuer_address_details.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'package:ribn/constants/assets.dart';
+import 'package:ribn/constants/strings.dart';
+import 'package:ribn/constants/styles.dart';
+import 'package:ribn/utils.dart';
+import 'package:ribn/widgets/custom_copy_button.dart';
+import 'package:ribn/widgets/custom_tooltip.dart';
+
+class IssuerAddressDetails extends StatelessWidget {
+  final String issuerAddress;
+  const IssuerAddressDetails({
+    Key? key,
+    required this.issuerAddress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: const [
+            Text(Strings.issuerAddress, style: RibnTextStyles.extH4),
+            CustomToolTip(tooltipText: 'Issuer Address'),
+          ],
+        ),
+        const SizedBox(height: 3),
+        Row(
+          children: [
+            SvgPicture.asset(RibnAssets.issuerFingerprint),
+            Text(
+              formatAddrString(issuerAddress),
+              style: RibnTextStyles.smallBody,
+            ),
+            CustomCopyButton(textToBeCopied: issuerAddress),
+          ],
+        ),
+      ],
+    );
+  }
+}
