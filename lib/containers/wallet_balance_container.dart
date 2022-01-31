f758312c34e1e6dcff3bec85a8d7f0d83a458a68
Mustafa Ali <mustafaali@toplmbp.lan>
2022-01-31 09:54:01 -0600

    Update wallet balance page

--- lib/containers/wallet_balance_container.dart
+++ lib/containers/wallet_balance_container.dart
@@ -3,9 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
+
 import 'package:ribn/actions/misc_actions.dart';
 import 'package:ribn/constants/routes.dart';
 import 'package:ribn/models/app_state.dart';
+import 'package:ribn/models/asset_details.dart';
 
 /// Intended to wrap the [WalletBalancePage] and provide it with the the [WalletBalanceViewModel].
 class WalletBalanceContainer extends StatelessWidget {
@@ -25,10 +27,12 @@ class WalletBalanceContainer extends StatelessWidget {
 class WalletBalanceViewModel {
   final num polyBalance;
   final List<AssetAmount> assets;
+  final Map<String, AssetDetails> assetDetails;
   final Function(AssetAmount) initiateSendAsset;
   final Function() initiateSendPolys;
   final bool failedToFetchBalances;
   final bool fetchingBalances;
+  final Function(AssetAmount) viewAssetDetails;
 
   WalletBalanceViewModel({
     required this.polyBalance,
@@ -37,17 +41,28 @@ class WalletBalanceViewModel {
     required this.initiateSendPolys,
     required this.failedToFetchBalances,
     required this.fetchingBalances,
+    required this.viewAssetDetails,
+    required this.assetDetails,
   });
   static WalletBalanceViewModel fromStore(Store<AppState> store) {
     return WalletBalanceViewModel(
       polyBalance: store.state.keychainState.currentNetwork.getPolysInWallet(),
       assets: store.state.keychainState.currentNetwork.getAllAssetsInWallet(),
+      assetDetails: store.state.userDetailsState.assetDetails,
       initiateSendAsset: (AssetAmount asset) => store.dispatch(
         NavigateToRoute(Routes.assetTransferInput, arguments: asset),
       ),
       initiateSendPolys: () => store.dispatch(NavigateToRoute(Routes.polyTransferInput)),
       failedToFetchBalances: store.state.uiState.failedToFetchBalances,
       fetchingBalances: store.state.uiState.fetchingBalances,
+      viewAssetDetails: (AssetAmount assetAmount) => store.dispatch(
+        NavigateToRoute(
+          Routes.assetDetails,
+          arguments: {
+            'assetAmount': assetAmount,
+          },
+        ),
+      ),
     );
   }
 
@@ -61,7 +76,8 @@ class WalletBalanceViewModel {
         other.initiateSendAsset == initiateSendAsset &&
         other.initiateSendPolys == initiateSendPolys &&
         other.failedToFetchBalances == failedToFetchBalances &&
-        other.fetchingBalances == fetchingBalances;
+        other.fetchingBalances == fetchingBalances &&
+        other.viewAssetDetails == viewAssetDetails;
   }
 
   @override
@@ -71,6 +87,7 @@ class WalletBalanceViewModel {
         initiateSendAsset.hashCode ^
         initiateSendPolys.hashCode ^
         failedToFetchBalances.hashCode ^
-        fetchingBalances.hashCode;
+        fetchingBalances.hashCode ^
+        viewAssetDetails.hashCode;
   }
 }
