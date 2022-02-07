ad11a160505d7f7b21c482eb7c7089d1bfe2d7e3
Mustafa Ali <mustafaali@toplmbp.lan>
2022-02-07 16:00:52 -0600

    Update background script

--- web/src/scripts/background.ts
+++ web/src/scripts/background.ts
@@ -1,24 +1,11 @@
 import { Messenger } from "../api/messenger";
-import { ExtensionStorage } from "../api/storage";
+// import { ExtensionStorage } from "../api/storage";
 
 /**
  * Event fired upon initial installation of the extension
  */
 chrome.runtime.onInstalled.addListener((details: chrome.runtime.InstalledDetails) => {
 	console.log("Extension installed:", details);
-});
-
-/**
- * Opens a new tab if onboarding, otherwise sets the extension popup.
- */
-chrome.action.onClicked.addListener(async (tab: chrome.tabs.Tab) => {
-	const storage = await ExtensionStorage.getStorage();
-	if (!storage || !storage["keychainState"] || !storage["keychainState"]["keyStoreJson"]) {
-		chrome.tabs.create({ url: chrome.runtime.getURL("index.html"), active: true });
-	} else {
-		chrome.action.setPopup({ popup: chrome.runtime.getURL("index.html") });
-	}
-	console.log(tab);
 });
 
 const messenger = Messenger.createBackgroundMessagingController();
