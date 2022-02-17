94790e9f8b3ed6935c8599d4c8360b51c8f8b66c
Mustafa Ali <mustafaali@toplmbp.lan>
2022-02-17 13:26:30 -0800

    Update restore with mnemonic page

--- lib/presentation/login/login_restore_with_mnemonic_page.dart
+++ lib/presentation/login/login_restore_with_mnemonic_page.dart
@@ -4,7 +4,6 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:ribn/actions/misc_actions.dart';
 import 'package:ribn/constants/colors.dart';
 import 'package:ribn/constants/routes.dart';
-import 'package:ribn/constants/rules.dart';
 import 'package:ribn/constants/strings.dart';
 import 'package:ribn/constants/styles.dart';
 import 'package:ribn/models/app_state.dart';
@@ -12,17 +11,18 @@ import 'package:ribn/presentation/login/widgets/next_button.dart';
 import 'package:ribn/presentation/login/widgets/restore_page_title.dart';
 import 'package:ribn/widgets/custom_text_field.dart';
 
-/// This page allows the user to enter a known seed phrase in order to restore a wallet.
+/// This page allows the user to enter a known mnemonic / seed phrase in order to restore a wallet.
 ///
-/// This page is used in the 'restore wallet' flow when it is initiated from the login page.
-class LoginRestoreWithSeedPhrasePage extends StatefulWidget {
-  const LoginRestoreWithSeedPhrasePage({Key? key}) : super(key: key);
+/// This page is used in the 'restore wallet' flow when initiated from the login page,
+/// hence the widget name is prefixed with 'Login'.
+class LoginRestoreWithMnemonicPage extends StatefulWidget {
+  const LoginRestoreWithMnemonicPage({Key? key}) : super(key: key);
 
   @override
-  _LoginRestoreWithSeedPhrasePageState createState() => _LoginRestoreWithSeedPhrasePageState();
+  _LoginRestoreWithMnemonicPageState createState() => _LoginRestoreWithMnemonicPageState();
 }
 
-class _LoginRestoreWithSeedPhrasePageState extends State<LoginRestoreWithSeedPhrasePage> {
+class _LoginRestoreWithMnemonicPageState extends State<LoginRestoreWithMnemonicPage> {
   /// Controller for the seed phrase text field.
   final TextEditingController controller = TextEditingController();
 
@@ -78,29 +78,30 @@ class _LoginRestoreWithSeedPhrasePageState extends State<LoginRestoreWithSeedPhr
               hasError: invalidSeedPhraseEntered,
             ),
             const Spacer(),
-            NextButton(
-              onPressed: () {
-                final bool isSeedPhraseValid = validateMnemonic(seedPhrase, 'english');
-                if (isSeedPhraseValid) {
-                  StoreProvider.of<AppState>(context).dispatch(
-                    NavigateToRoute(
-                      Routes.restoreWalletnewPassword,
-                      arguments: {
-                        RecoverWalletCreds.seedPhrase: seedPhrase,
-                      },
-                    ),
-                  );
-                } else {
-                  setState(() {
-                    invalidSeedPhraseEntered = true;
-                  });
-                }
-              },
-            ),
+            NextButton(onPressed: onNextPressed),
             const SizedBox(height: 17),
           ],
         ),
       ),
     );
+  }
+
+  /// Handler for when [NextButton] is pressed.
+  ///
+  /// Validates the seedphrase entered and if valid, navigates to the next page, i.e. [Routes.loginRestoreWalletnewPassword].
+  void onNextPressed() {
+    final bool isSeedPhraseValid = validateMnemonic(seedPhrase, 'english');
+    if (isSeedPhraseValid) {
+      StoreProvider.of<AppState>(context).dispatch(
+        NavigateToRoute(
+          Routes.loginRestoreWalletnewPassword,
+          arguments: seedPhrase,
+        ),
+      );
+    } else {
+      setState(() {
+        invalidSeedPhraseEntered = true;
+      });
+    }
   }
 }
