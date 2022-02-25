e8b693a826b76ec1521e6ffe87a04dc896a194ca
Mustafa Ali <mustafaali@toplmbp.lan>
2022-02-25 15:15:20 -0600

    Add error handling

--- web/src/api/webpage.ts
+++ web/src/api/webpage.ts
@@ -2,23 +2,35 @@ import { API_METHODS } from "../utils/configs";
 import { Messenger } from "./messenger";
 
 export const signTx = async (rawTx: any) => {
-	const result = await Messenger.forwardToContentScript({
-		method: API_METHODS.signTx,
-		data: rawTx,
-	});
-	return result.data;
+	try {
+		const result = await Messenger.forwardToContentScript({
+			method: API_METHODS.signTx,
+			data: rawTx,
+		});
+		return result.data;
+	} catch (err) {
+		return err;
+	}
 };
 
 export const enable = async () => {
-	const result = await Messenger.forwardToContentScript({
-		method: API_METHODS.enable,
-	});
-	return result.data;
+	try {
+		const result = await Messenger.forwardToContentScript({
+			method: API_METHODS.enable,
+		});
+		return result.data;
+	} catch (err) {
+		return err;
+	}
 };
 
 export const isEnabled = async () => {
-	const result = await Messenger.forwardToContentScript({
-		method: API_METHODS.isEnabled,
-	});
-	return result.data;
+	try {
+		const result = await Messenger.forwardToContentScript({
+			method: API_METHODS.isEnabled,
+		});
+		return result.data;
+	} catch (err) {
+		return err;
+	}
 };
