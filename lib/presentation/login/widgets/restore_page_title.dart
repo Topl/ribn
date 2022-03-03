baff86867b27f19014ef5bc56a44365215fcfe84
Mustafa Ali <mustafaali@toplmbp.lan>
2022-03-03 13:20:41 -0600

    Cleanup

--- lib/presentation/login/widgets/restore_page_title.dart
+++ lib/presentation/login/widgets/restore_page_title.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:ribn/presentation/login/widgets/page_indicator.dart';
 import 'package:ribn/widgets/custom_page_title.dart';
 
-/// The page title, along with the [CustomPageIndicator], that is displayed when going through the 'restore wallet' flow.
+/// The page title that is displayed when going through the 'restore wallet' flow.
 class RestoreWalletPageTitle extends StatelessWidget {
-  final int currPage;
-  const RestoreWalletPageTitle({required this.currPage, Key? key}) : super(key: key);
+  const RestoreWalletPageTitle({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +11,9 @@ class RestoreWalletPageTitle extends StatelessWidget {
       padding: const EdgeInsets.only(top: 40),
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        children: [
-          const CustomPageTitle(title: 'Restore Wallet'),
-          const SizedBox(height: 17),
-          Center(
-            child: CustomPageIndicator(
-              currPage: currPage,
-            ),
-          ),
+        children: const [
+          CustomPageTitle(title: 'Restore Wallet'),
+          SizedBox(height: 17),
         ],
       ),
     );
