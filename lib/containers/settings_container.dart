5a49823412d529f268c45811ca19456e72bd11d4
Mustafa Ali <mustafaali@toplmbp.lan>
2022-03-08 13:09:54 -0600

    Update settings container

--- lib/containers/settings_container.dart
+++ lib/containers/settings_container.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
@@ -5,6 +7,7 @@ import 'package:redux/redux.dart';
 import 'package:ribn/actions/misc_actions.dart';
 import 'package:ribn/models/app_state.dart';
 
+/// Intended to wrap the [SettingsPage] and provide it with the the [SettingsViewModel].
 class SettingsContainer extends StatelessWidget {
   const SettingsContainer({Key? key, required this.builder}) : super(key: key);
   final ViewModelBuilder<SettingsViewModel> builder;
@@ -20,16 +23,47 @@ class SettingsContainer extends StatelessWidget {
 }
 
 class SettingsViewModel {
-  final VoidCallback exportKeyStore;
-  final VoidCallback deleteWallet;
+  /// Callback to download Topl Main Key
+  final VoidCallback exportToplMainKey;
+
+  /// Handler for deleting the wallet.
+  final void Function({
+    required String password,
+    required VoidCallback onIncorrectPasswordEntered,
+  }) deleteWallet;
+
+  /// The current app version.
+  final String appVersion;
+
   SettingsViewModel({
-    required this.exportKeyStore,
+    required this.exportToplMainKey,
     required this.deleteWallet,
+    required this.appVersion,
   });
   static SettingsViewModel fromStore(Store<AppState> store) {
     return SettingsViewModel(
-      exportKeyStore: () => store.dispatch(DownloadAsFile('keystore.json', store.state.keychainState.keyStoreJson!)),
-      deleteWallet: () => store.dispatch(const DeleteWalletAction()),
+      exportToplMainKey: () => store.dispatch(
+        DownloadAsFileAction(
+          'topl_main_key.json',
+          store.state.keychainState.keyStoreJson!,
+        ),
+      ),
+      deleteWallet: ({
+        required String password,
+        required VoidCallback onIncorrectPasswordEntered,
+      }) {
+        final Completer<bool> actionCompleter = Completer();
+        store.dispatch(
+          DeleteWalletAction(
+            password: password,
+            completer: actionCompleter,
+          ),
+        );
+        actionCompleter.future.asStream().listen((event) {
+          if (event == false) onIncorrectPasswordEntered();
+        });
+      },
+      appVersion: store.state.appVersion,
     );
   }
 
@@ -37,9 +71,9 @@ class SettingsViewModel {
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is SettingsViewModel && other.exportKeyStore == exportKeyStore && other.deleteWallet == deleteWallet;
+    return other is SettingsViewModel && other.exportToplMainKey == exportToplMainKey && other.appVersion == appVersion;
   }
 
   @override
-  int get hashCode => exportKeyStore.hashCode ^ deleteWallet.hashCode;
+  int get hashCode => exportToplMainKey.hashCode ^ appVersion.hashCode;
 }
