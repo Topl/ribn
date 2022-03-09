c51071a8ada568371967606275a2692285127384
Mustafa Ali <mustafaali@toplmbp.lan>
2022-03-09 10:55:21 -0600

    Update restore wallet flows

--- lib/presentation/onboarding/restore_wallet/onboarding_enter_wallet_password_page.dart
+++ lib/presentation/onboarding/restore_wallet/onboarding_enter_wallet_password_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/svg.dart';
@@ -6,7 +8,6 @@ import 'package:ribn/constants/assets.dart';
 import 'package:ribn/constants/strings.dart';
 import 'package:ribn/constants/styles.dart';
 import 'package:ribn/models/app_state.dart';
-import 'package:ribn/models/ui_state.dart';
 import 'package:ribn/presentation/login/widgets/next_button.dart';
 import 'package:ribn/presentation/login/widgets/password_text_field.dart';
 import 'package:ribn/widgets/onboarding_app_bar.dart';
@@ -27,6 +28,7 @@ class OnboardingEnterWalletPasswordPage extends StatefulWidget {
 class _OnboardingEnterWalletPasswordPageState extends State<OnboardingEnterWalletPasswordPage> {
   final double maxWidth = 734;
   final TextEditingController _passwordController = TextEditingController();
+  bool _failedToRestoreWallet = false;
 
   @override
   void initState() {
@@ -38,44 +40,48 @@ class _OnboardingEnterWalletPasswordPageState extends State<OnboardingEnterWalle
 
   @override
   Widget build(BuildContext context) {
-    return StoreConnector<AppState, UiState>(
-      converter: (store) => store.state.uiState,
-      builder: (context, vm) => Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              OnboardingAppBar(onBackPressed: () => Navigator.of(context).pop()),
-              _buildTitle(),
-              SizedBox(
-                width: maxWidth,
-                child: const Text(
-                  Strings.enterWalletPasswordToRestoreWallet,
-                ),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            OnboardingAppBar(onBackPressed: () => Navigator.of(context).pop()),
+            _buildTitle(),
+            SizedBox(
+              width: maxWidth,
+              child: const Text(
+                Strings.enterWalletPasswordToRestoreWallet,
               ),
-              // warning section
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 24.0),
-                child: _buildReadCarefullySection(),
-              ),
-              _buildEnterWalletPassword(),
-              vm.failedToRestoreWallet
-                  ? const Text(
-                      'Failed to restore wallet.',
-                      style: TextStyle(color: Colors.red),
-                    )
-                  : const SizedBox(),
-              NextButton(
-                onPressed: () {
-                  StoreProvider.of<AppState>(context).dispatch(
-                    RestoreWalletWithToplKeyAction(
-                      toplKeyStoreJson: widget.toplKeyStoreJson,
-                      password: _passwordController.text,
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
+            ),
+            // warning section
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24.0),
+              child: _buildReadCarefullySection(),
+            ),
+            _buildEnterWalletPassword(),
+            _failedToRestoreWallet
+                ? const Text(
+                    'Failed to restore wallet.',
+                    style: TextStyle(color: Colors.red),
+                  )
+                : const SizedBox(),
+            NextButton(
+              onPressed: () async {
+                final Completer<bool> actionCompleter = Completer();
+                StoreProvider.of<AppState>(context).dispatch(
+                  RestoreWalletWithToplKeyAction(
+                    toplKeyStoreJson: widget.toplKeyStoreJson,
+                    password: _passwordController.text,
+                    completer: actionCompleter,
+                  ),
+                );
+                await actionCompleter.future.then((value) {
+                  setState(() {
+                    _failedToRestoreWallet = !value;
+                  });
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
