abbd3190445d0c06a6dca1c81b6355531bc000d2
Mustafa Ali <mustafaali@toplmbp.lan>
2022-03-14 11:27:40 -0500

    Update keychainstate

--- lib/models/keychain_state.dart
+++ lib/models/keychain_state.dart
@@ -1,75 +1,94 @@
 import 'dart:convert';
 import 'dart:typed_data';
-import 'package:bip_topl/bip_topl.dart' as t;
+
+import 'package:bip_topl/bip_topl.dart' as bip_topl;
 import 'package:brambldart/credentials.dart';
 import 'package:flutter/foundation.dart';
+
+import 'package:ribn/constants/network_utils.dart';
 import 'package:ribn/constants/rules.dart';
 import 'package:ribn/constants/test_data.dart';
 import 'package:ribn/models/ribn_network.dart';
 
+@immutable
 class KeychainState {
+  /// The encrypted Topl Main Key as json. Can be null, e.g. before onboarding.
   final String? keyStoreJson;
-  final HdWallet? hdWallet; // never persisted
-  final List<RibnNetwork> networks;
-  final int currNetworkIdx;
 
-  RibnNetwork get currentNetwork => networks[currNetworkIdx];
+  /// An HdWallet helper to derive child keys.
+  final HdWallet? hdWallet;
 
-  KeychainState({
+  /// Networks supported by Ribn, with the networkName as key and [RibnNetwork] as value.
+  final Map<String, RibnNetwork> networks;
+
+  /// The name of the current network.
+  final String currentNetworkName;
+
+  /// Gets the current [RibnNetwork].
+  RibnNetwork get currentNetwork => networks[currentNetworkName]!;
+
+  /// Gets all supported [RibnNetwork]s.
+  List<RibnNetwork> get allNetworks => networks.values.toList();
+
+  const KeychainState({
     this.keyStoreJson,
     this.hdWallet,
     required this.networks,
-    required this.currNetworkIdx,
+    required this.currentNetworkName,
   });
 
   factory KeychainState.initial() {
-    final List<RibnNetwork> initialNetworks = RibnNetwork.initializeNetworks();
     return KeychainState(
-      networks: initialNetworks,
-      currNetworkIdx: 0,
+      currentNetworkName: NetworkUtils.valhalla,
+      networks: Map.unmodifiable(RibnNetwork.initializeToplNetworks()),
     );
   }
 
   factory KeychainState.test() {
     final Uint8List toplExtendedPrvKeyUint8List = TestData.toplExtendedPrvKeyUint8List;
-    final List<RibnNetwork> initialNetworks = RibnNetwork.initializeNetworks();
     return KeychainState(
       keyStoreJson: TestData.keyStoreJson,
-      networks: initialNetworks,
-      currNetworkIdx: 0,
+      currentNetworkName: NetworkUtils.valhalla,
       hdWallet: HdWallet(
-        rootSigningKey: t.Bip32SigningKey.fromValidBytes(
+        rootSigningKey: bip_topl.Bip32SigningKey.fromValidBytes(
           toplExtendedPrvKeyUint8List,
           depth: Rules.toplKeyDepth,
         ),
       ),
+      networks: Map.unmodifiable(RibnNetwork.initializeToplNetworks()),
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'keyStoreJson': keyStoreJson,
-      'networks': networks.map((x) => x.toMap()).toList(),
-      'currNetworkIdx': currNetworkIdx,
+      'currentNetworkId': currentNetworkName,
+      'networks': networks.map((key, value) => MapEntry(key, value.toMap())),
     };
   }
 
   factory KeychainState.fromMap(Map<String, dynamic> map) {
     return KeychainState(
       keyStoreJson: map['keyStoreJson'] as String?,
-      networks: List<RibnNetwork>.from(
-          (map['networks'] as Iterable<dynamic>).map((x) => RibnNetwork.fromMap(x as Map<String, dynamic>))),
-      currNetworkIdx: map['currNetworkIdx'] as int,
+      currentNetworkName: map['currentNetworkName'] as String,
+      networks: Map<String, RibnNetwork>.from(
+        (map['networks'] as Map<String, dynamic>).map(
+          (key, value) => MapEntry(
+            key,
+            RibnNetwork.fromMap(value as Map<String, dynamic>),
+          ),
+        ),
+      ),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory KeychainState.fromJson(String source) => KeychainState.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory KeychainState.fromJson(String source) => KeychainState.fromMap(json.decode(source));
 
   @override
   String toString() {
-    return 'KeychainState(keyStoreJson: $keyStoreJson, hdWallet: $hdWallet, networks: $networks, currNetworkIdx: $currNetworkIdx)';
+    return 'KeychainState(keyStoreJson: $keyStoreJson, hdWallet: $hdWallet, networks: $networks, currentNetworkName: $currentNetworkName)';
   }
 
   @override
@@ -79,26 +98,26 @@ class KeychainState {
     return other is KeychainState &&
         other.keyStoreJson == keyStoreJson &&
         other.hdWallet == hdWallet &&
-        listEquals(other.networks, networks) &&
-        other.currNetworkIdx == currNetworkIdx;
+        mapEquals(other.networks, networks) &&
+        other.currentNetworkName == currentNetworkName;
   }
 
   @override
   int get hashCode {
-    return keyStoreJson.hashCode ^ hdWallet.hashCode ^ networks.hashCode ^ currNetworkIdx.hashCode;
+    return keyStoreJson.hashCode ^ hdWallet.hashCode ^ networks.hashCode ^ currentNetworkName.hashCode;
   }
 
   KeychainState copyWith({
     String? keyStoreJson,
     HdWallet? hdWallet,
-    List<RibnNetwork>? networks,
-    int? currNetworkIdx,
+    Map<String, RibnNetwork>? networks,
+    String? currentNetworkName,
   }) {
     return KeychainState(
       keyStoreJson: keyStoreJson ?? this.keyStoreJson,
       hdWallet: hdWallet ?? this.hdWallet,
       networks: networks ?? this.networks,
-      currNetworkIdx: currNetworkIdx ?? this.currNetworkIdx,
+      currentNetworkName: currentNetworkName ?? this.currentNetworkName,
     );
   }
 }
