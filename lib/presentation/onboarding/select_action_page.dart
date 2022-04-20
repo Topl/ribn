679b7a7a3aa4e31ff37ea41f00b246f2eda509b5
OlliePorter <ol.porter@me.com>
2022-04-20 16:58:50 -0500

    Replace onboarding selection wallet action with SquareIconButton from Ribn Toolkit

--- lib/presentation/onboarding/select_action_page.dart
+++ lib/presentation/onboarding/select_action_page.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:ribn/actions/misc_actions.dart';
-import 'package:ribn/constants/assets.dart';
 import 'package:ribn/constants/colors.dart';
 import 'package:ribn/constants/keys.dart';
 import 'package:ribn/constants/routes.dart';
 import 'package:ribn/constants/strings.dart';
-import 'package:ribn/constants/styles.dart';
 import 'package:ribn/models/app_state.dart';
 import 'package:ribn/widgets/onboarding_app_bar.dart';
+import 'package:ribn_toolkit/constants/assets.dart';
+import 'package:ribn_toolkit/constants/styles.dart';
+import 'package:ribn_toolkit/widgets/atoms/square_icon_button.dart';
 
 /// Allow the user to select from 'create wallet' or 'import wallet'.
 class SelectActionPage extends StatelessWidget {
@@ -57,59 +58,12 @@ class SelectActionPage extends StatelessWidget {
 
     return Column(
       children: [
-        SizedBox(
-          height: 220,
-          width: 320,
-          child: OutlinedButton(
-            style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(11),
-                ),
-              ),
-              minimumSize: MaterialStateProperty.all(
-                const Size(285, 165),
-              ),
-              padding: MaterialStateProperty.all<EdgeInsets>(
-                const EdgeInsets.all(0),
-              ),
-              backgroundColor: MaterialStateProperty.all(RibnColors.primary),
-            ),
-            onPressed: () => StoreProvider.of<AppState>(context).dispatch(
-              NavigateToRoute(navigateToRoute),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30, right: 30),
-                    child: Container(
-                      width: 60,
-                      height: 60,
-                      decoration: const BoxDecoration(
-                        color: Color(0xffB1E7E1),
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(50),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(18),
-                        child: SvgPicture.asset(iconToDisplay, width: 30),
-                      ),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 30, bottom: 30),
-                    child: Text(title, style: RibnTextStyles.btnLarge),
-                  ),
-                ),
-              ],
-            ),
+        SquareIconButton(
+          backgroundColor: RibnColors.primary,
+          icon: SvgPicture.asset(iconToDisplay, width: 30),
+          text: Text(title, style: RibnTextStyles.btnLarge),
+          onPressed: () => StoreProvider.of<AppState>(context).dispatch(
+            NavigateToRoute(navigateToRoute),
           ),
         ),
         const SizedBox(height: 40),
