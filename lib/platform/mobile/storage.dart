afd57ae4c3e0abe46f7bf492350387e837a5ecc1
mustafalii <mustafaali.py@gmail.com>
2022-05-05 17:04:09 -0500

    Add platform storage methods

--- lib/platform/mobile/storage.dart
+++ lib/platform/mobile/storage.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:ribn/platform/interfaces.dart';
 
@@ -12,6 +13,11 @@ class PlatformLocalStorage implements IPlatformLocalStorage {
   }
   static PlatformLocalStorage get instance => PlatformLocalStorage();
 
+  /// Allows securely storing credentials on mobile.
+  /// Uses keychain for iOS; AES encryption and KeyStore for android
+  /// Ref: https://pub.dev/packages/flutter_secure_storage
+  final FlutterSecureStorage secureStorage = const FlutterSecureStorage();
+
   @override
   Future<String> getState() async {
     final File file = File('${(await getApplicationDocumentsDirectory()).path}/app_state.json');
@@ -22,5 +28,15 @@ class PlatformLocalStorage implements IPlatformLocalStorage {
   Future<void> saveState(String data) async {
     final file = File('${(await getApplicationDocumentsDirectory()).path}/app_state.json');
     await file.writeAsString(data, flush: true);
+  }
+
+  @override
+  Future<String?> getSessionKey() {
+    return secureStorage.read(key: 'toplKey');
+  }
+
+  @override
+  Future<void> saveSessionKey(String key) async {
+    await secureStorage.write(key: 'toplKey', value: key);
   }
 }
