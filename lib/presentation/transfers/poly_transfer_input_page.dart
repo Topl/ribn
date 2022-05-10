dd57ced10fe7bccdd1c35ddbe8d71cbcc14d523e
mustafalii <mustafaali.py@gmail.com>
2022-05-10 16:13:43 -0500

    Update validation for poly transfer

--- lib/presentation/transfers/poly_transfer_input_page.dart
+++ lib/presentation/transfers/poly_transfer_input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ribn/constants/assets.dart';
 import 'package:ribn/constants/colors.dart';
 import 'package:ribn/constants/strings.dart';
@@ -37,6 +38,9 @@ class _PolyTransferInputPageState extends State<PolyTransferInputPage> {
 
   /// True if currently loading raw tx creation.
   bool _loadingRawTx = false;
+
+  /// True if invalid amount entered.
+  bool _invalidAmount = false;
 
   @override
   void initState() {
@@ -94,7 +98,7 @@ class _PolyTransferInputPageState extends State<PolyTransferInputPage> {
                                 _buildPolyDisplay(),
                                 const SizedBox(width: 20),
                                 // field for entering amount of polys needed for transfer
-                                _buildAmountField(),
+                                _buildAmountField(vm),
                               ],
                             ),
                             // field for displaying the sender addresss
@@ -118,10 +122,14 @@ class _PolyTransferInputPageState extends State<PolyTransferInputPage> {
                                   });
                                 },
                               ),
-                              // clear the textfield on backspace pressed
                               onBackspacePressed: () {
                                 setState(() {
-                                  _recipientController.clear();
+                                  if (_validRecipientAddress.isNotEmpty) {
+                                    _recipientController.text = _validRecipientAddress;
+                                    _recipientController
+                                      ..text = _recipientController.text.substring(0, _recipientController.text.length)
+                                      ..selection = TextSelection.collapsed(offset: _recipientController.text.length);
+                                  }
                                   _validRecipientAddress = '';
                                 });
                               },
@@ -185,42 +193,58 @@ class _PolyTransferInputPageState extends State<PolyTransferInputPage> {
   }
 
   /// Builds the TextField for entering amount needed for the transfer.
-  Widget _buildAmountField() {
+  Widget _buildAmountField(PolyTransferInputViewModel vm) {
     return CustomInputField(
       itemLabel: Strings.amount,
       item: CustomTextField(
+        hasError: _invalidAmount,
+        onChanged: (amount) {
+          setState(() {
+            if ((int.tryParse(amount) ?? 0) > vm.maxTransferrableAmount) {
+              _invalidAmount = true;
+            } else {
+              _invalidAmount = false;
+            }
+          });
+        },
         width: 82,
         height: 31,
         controller: _amountController,
         hintText: Strings.amountHint,
+        keyboardType: TextInputType.number,
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
       ),
     );
   }
 
   /// Builds the review button to initate tx.
   Widget _buildReviewButton(PolyTransferInputViewModel vm) {
+    final bool enteredValidInputs =
+        _validRecipientAddress.isNotEmpty && _amountController.text.isNotEmpty && !_invalidAmount;
     return Padding(
       padding: const EdgeInsets.only(top: 20.0, bottom: 10),
       child: LargeButton(
         label: Strings.review,
-        onPressed: () {
-          setState(() {
-            _loadingRawTx = true;
-          });
-          vm.initiateTx(
-            amount: _amountController.text,
-            recipient: _validRecipientAddress,
-            note: _noteController.text,
-            onRawTxCreated: (bool success) async {
-              _loadingRawTx = false;
-              setState(() {});
-              // Display error dialog if failed to create raw tx
-              if (!success) {
-                await TransferUtils.showErrorDialog(context);
+        onPressed: enteredValidInputs
+            ? () {
+                setState(() {
+                  _loadingRawTx = true;
+                });
+                vm.initiateTx(
+                  amount: _amountController.text,
+                  recipient: _validRecipientAddress,
+                  note: _noteController.text,
+                  onRawTxCreated: (bool success) async {
+                    _loadingRawTx = false;
+                    setState(() {});
+                    // Display error dialog if failed to create raw tx
+                    if (!success) {
+                      await TransferUtils.showErrorDialog(context);
+                    }
+                  },
+                );
               }
-            },
-          );
-        },
+            : null,
       ),
     );
   }
