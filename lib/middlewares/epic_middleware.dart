371308841cba1e5545cb912c65b5f3c863215b36
mustafalii <mustafaali.py@gmail.com>
2022-05-19 11:18:29 -0500

    Fix wallet creation

--- lib/middlewares/epic_middleware.dart
+++ lib/middlewares/epic_middleware.dart
@@ -27,7 +27,6 @@ const List<dynamic> persistenceTriggers = [
   AddAddressAction,
   UpdateCurrentNetworkAction,
   UpdateBalancesAction,
-  InitializeHDWalletAction,
   UpdateAssetDetailsAction,
 ];
 
