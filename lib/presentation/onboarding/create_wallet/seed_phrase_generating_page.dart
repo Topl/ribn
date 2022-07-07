7735ec8b687d14bad7ec4e963d3fdef177e87c0d
mustafalii <mustafaali.py@gmail.com>
2022-07-07 14:28:53 -0500

    Update animated loader widget

--- lib/presentation/onboarding/create_wallet/seed_phrase_generating_page.dart
+++ lib/presentation/onboarding/create_wallet/seed_phrase_generating_page.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -13,8 +11,8 @@ import 'package:ribn/presentation/onboarding/widgets/confirmation_button.dart';
 import 'package:ribn/presentation/onboarding/widgets/onboarding_container.dart';
 import 'package:ribn/presentation/onboarding/widgets/web_onboarding_app_bar.dart';
 import 'package:ribn/utils.dart';
-import 'package:ribn_toolkit/constants/colors.dart';
 import 'package:ribn_toolkit/constants/styles.dart';
+import 'package:ribn_toolkit/widgets/molecules/animated_circle_step_loader.dart';
 
 /// This page shows a loading animation to indicate seed phrase generation.
 class SeedPhraseGeneratingPage extends StatefulWidget {
@@ -25,45 +23,10 @@ class SeedPhraseGeneratingPage extends StatefulWidget {
 }
 
 class _SeedPhraseGeneratingPageState extends State<SeedPhraseGeneratingPage> {
-  /// Timer for the animation, initialized in [initState].
-  late final Timer timer;
-
-  /// Duration for animating circles.
-  final Duration duration = const Duration(seconds: 2);
-
-  /// Number of circles in the loading animation.
-  final int numCircles = 5;
-
   /// True if animated loader needs to be shown.
   bool seedPhraseGenerating = true;
 
-  /// Current active circle position.
-  int currCircle = 0;
-
-  final List<int> circlePositions = List.generate(5, (idx) => idx).toList();
-  final double smallRadius = 4.5;
-  final double bigRadius = 8;
-
   final double descriptionBoxWidth = kIsWeb ? 640 : 350;
-
-  @override
-  void initState() {
-    timer = Timer.periodic(duration, (timer) {
-      if (timer.tick == numCircles) {
-        seedPhraseGenerating = false;
-        timer.cancel();
-      }
-      currCircle = (currCircle + 1) % numCircles;
-      setState(() {});
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -87,19 +50,19 @@ class _SeedPhraseGeneratingPageState extends State<SeedPhraseGeneratingPage> {
   /// List of widgets displayed when seed phrase is being generated.
   List<Widget> seedPhraseGeneratingSection() {
     return [
-      SizedBox(
-        width: kIsWeb ? double.infinity : 312,
-        child: _buildTitle(currCircle),
-      ),
-      Padding(
-        padding: const EdgeInsets.only(top: 50.0, bottom: 70),
-        child: SizedBox(
-          height: 30,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: circlePositions.map(_buildAnimatedContainer).toList(),
-          ),
-        ),
+      AnimatedCircleStepLoader(
+        stepLabels: const {
+          0: Strings.seedPhraseGenerating,
+          1: Strings.goGrabAPenAndPaper,
+          2: Strings.seriouslyGetAPenAndPaper,
+          3: Strings.seedPhraseGenerating,
+          4: Strings.seedPhraseGenerating,
+        },
+        showStepLoader: () {
+          setState(() {
+            seedPhraseGenerating = false;
+          });
+        },
       ),
       SizedBox(
         width: descriptionBoxWidth,
@@ -143,52 +106,5 @@ class _SeedPhraseGeneratingPageState extends State<SeedPhraseGeneratingPage> {
         ),
       ),
     ];
-  }
-
-  /// Builds title based on active circle [position].
-  Widget _buildTitle(int position) {
-    switch (position) {
-      case 0:
-      case 3:
-      case 4:
-        {
-          return const Text(
-            Strings.seedPhraseGenerating,
-            style: RibnToolkitTextStyles.onboardingH1,
-            textAlign: TextAlign.center,
-          );
-        }
-      case 1:
-        {
-          return const Text(
-            Strings.goGrabAPenAndPaper,
-            style: RibnToolkitTextStyles.onboardingH1,
-            textAlign: TextAlign.center,
-          );
-        }
-      case 2:
-        {
-          return const Text(
-            Strings.seriouslyGetAPenAndPaper,
-            style: RibnToolkitTextStyles.onboardingH1,
-            textAlign: TextAlign.center,
-          );
-        }
-    }
-    return const Text('');
-  }
-
-  /// Animate color and radius of the active circles.
-  Widget _buildAnimatedContainer(int position) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: AnimatedContainer(
-        duration: duration,
-        child: CircleAvatar(
-          backgroundColor: position <= currCircle ? RibnColors.primary : RibnColors.inactive,
-          radius: currCircle == position ? bigRadius : smallRadius,
-        ),
-      ),
-    );
   }
 }
