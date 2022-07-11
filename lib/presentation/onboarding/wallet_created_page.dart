9d8e2fa168de1d2925553fad8552a3dc51791700
OlliePorter <ol.porter@me.com>
2022-07-11 13:18:19 -0500

    Delete files again after merge conflicts which were deleted by Mustafa

--- lib/presentation/onboarding/wallet_created_page.dart
+++ lib/presentation/onboarding/wallet_created_page.dart
@@ -1,108 +1,112 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:ribn/actions/misc_actions.dart';
-import 'package:ribn_toolkit/constants/assets.dart';
-import 'package:ribn_toolkit/constants/colors.dart';
+import 'package:ribn/constants/assets.dart';
 import 'package:ribn/constants/routes.dart';
 import 'package:ribn/constants/strings.dart';
+import 'package:ribn/presentation/onboarding/utils.dart';
+import 'package:ribn/presentation/onboarding/widgets/confirmation_button.dart';
+import 'package:ribn/presentation/onboarding/widgets/mobile_onboarding_progress_bar.dart';
+import 'package:ribn/presentation/onboarding/widgets/onboarding_container.dart';
+import 'package:ribn/presentation/onboarding/widgets/web_onboarding_app_bar.dart';
+import 'package:ribn/utils.dart';
+import 'package:ribn_toolkit/constants/colors.dart';
 import 'package:ribn_toolkit/constants/styles.dart';
-import 'package:ribn/models/app_state.dart';
-import 'package:ribn_toolkit/widgets/atoms/large_button.dart';
 import 'package:ribn_toolkit/widgets/molecules/accordion.dart';
 
-/// Wallet created success page.
-/// Displays several FAQs to the user.
-class WalletCreatedPage extends StatelessWidget {
+/// This page is displayed when user successfully creates their wallet.
+class WalletCreatedPage extends StatefulWidget {
   const WalletCreatedPage({Key? key}) : super(key: key);
 
   @override
+  State<WalletCreatedPage> createState() => _WalletCreatedPageState();
+}
+
+class _WalletCreatedPageState extends State<WalletCreatedPage> {
+  /// FAQs and their corresponding answeres
+  final Map<String, String> faqs = {
+    Strings.howCanIKeepMySeedPhraseSecure: Strings.howCanIKeepMySeedPhraseSecureAns,
+    Strings.howIsASeedPhraseDifferent: Strings.howIsASeedPhraseDifferentAns,
+    Strings.howIsMySeedPhraseUnrecoverable: Strings.howIsMySeedPhraseUnrecoverableAns,
+  };
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 40),
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              width: 315,
-              height: 100,
-              child: Text(
+    return Scaffold(
+      body: OnboardingContainer(
+        child: SingleChildScrollView(
+          clipBehavior: Clip.none,
+          child: Column(
+            children: [
+              renderIfWeb(const WebOnboardingAppBar(currStep: 3)),
+              const Text(
                 Strings.walletCreated,
-                style: RibnToolkitTextStyles.h1,
+                style: RibnToolkitTextStyles.onboardingH1,
                 textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(height: 40),
-            SvgPicture.asset(RibnAssets.logoCardIcon),
-            const SizedBox(
-              height: 95,
-              width: 680,
-              child: Text(
-                Strings.walletCreatedDesc,
-                style: RibnToolkitTextStyles.body1,
-                textHeightBehavior: TextHeightBehavior(applyHeightToFirstAscent: false),
-              ),
-            ),
-            const SizedBox(height: 30),
-            LargeButton(
-              buttonChild: Text(
-                Strings.cont,
-                style: RibnToolkitTextStyles.btnLarge.copyWith(
-                  color: Colors.white,
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Image.asset(
+                  RibnAssets.successPng,
+                  width: 100,
                 ),
               ),
-              backgroundColor: RibnColors.primary,
-              hoverColor: RibnColors.primaryButtonHover,
-              dropShadowColor: RibnColors.primaryButtonShadow,
-              onPressed: () => StoreProvider.of<AppState>(context).dispatch(
-                NavigateToRoute(Routes.extensionInfo),
+              SizedBox(
+                width: 730,
+                child: Column(
+                  children: [
+                    const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        Strings.walletCreatedDesc,
+                        style: RibnToolkitTextStyles.onboardingH3,
+                      ),
+                    ),
+                    SizedBox(height: adaptHeight(0.05)),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        Strings.frequentlyAskedQuestions,
+                        style: RibnToolkitTextStyles.h3.copyWith(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: adaptHeight(0.01)),
+                    ...faqs.keys
+                        .map(
+                          (q) => Accordion(
+                            header: Text(
+                              q,
+                              style: RibnToolkitTextStyles.h3.copyWith(color: Colors.white),
+                            ),
+                            description: Text(
+                              faqs[q]!,
+                              style: RibnToolkitTextStyles.h4.copyWith(color: Colors.white),
+                            ),
+                            width: double.infinity,
+                            backgroundColor: RibnColors.primary,
+                            collapsedBackgroundColor: RibnColors.primary,
+                            // textColor: Colors.white,
+                            iconColor: Colors.white,
+                          ),
+                        )
+                        .toList(),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 60),
-            SizedBox(
-              width: 660,
-              child: Row(
-                children: [
-                  Text(
-                    Strings.frequentlyAskedQuestions,
-                    style: RibnToolkitTextStyles.body1Bold.copyWith(fontSize: 25),
-                    textAlign: TextAlign.left,
-                  ),
-                  const Spacer(),
-                ],
+              SizedBox(height: adaptHeight(0.01)),
+              renderIfMobile(const MobileOnboardingProgressBar(currStep: 3)),
+              ConfirmationButton(
+                text: Strings.done,
+                onPressed: () {
+                  if (kIsWeb) {
+                    navigateToRoute(context, Routes.extensionInfo);
+                  } else {
+                    Navigator.of(context).pushNamedAndRemoveUntil(Routes.home, (_) => false);
+                  }
+                },
               ),
-            ),
-            const SizedBox(height: 20),
-            const Accordion(
-              header: Strings.howCanIKeepMySeedPhraseSecure,
-              description: Strings.howCanIKeepMySeedPhraseSecureAns,
-              width: 660,
-              backgroundColor: Colors.white,
-              collapsedBackgroundColor: Colors.white,
-              textColor: RibnColors.defaultText,
-              iconColor: RibnColors.defaultText,
-            ),
-            const Accordion(
-              header: Strings.howIsASeedPhraseDifferent,
-              description: Strings.howIsASeedPhraseDifferentAns,
-              width: 660,
-              backgroundColor: Colors.white,
-              collapsedBackgroundColor: Colors.white,
-              textColor: RibnColors.defaultText,
-              iconColor: RibnColors.defaultText,
-            ),
-            const Accordion(
-              header: Strings.howIsMySeedPhraseUnrecoverable,
-              description: Strings.howIsMySeedPhraseUnrecoverableAns,
-              width: 660,
-              backgroundColor: Colors.white,
-              collapsedBackgroundColor: Colors.white,
-              textColor: RibnColors.defaultText,
-              iconColor: RibnColors.defaultText,
-            ),
-            const SizedBox(height: 20),
-          ],
+            ],
+          ),
         ),
       ),
     );
