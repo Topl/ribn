02c37a0f64906426a251d2abf151a336d6fd7658
mustafalii <mustafaali.py@gmail.com>
2022-07-26 13:46:05 -0500

    Update enter wallet password page

--- lib/presentation/onboarding/widgets/warning_section.dart
+++ lib/presentation/onboarding/widgets/warning_section.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:ribn/constants/assets.dart';
 import 'package:ribn/constants/strings.dart';
@@ -6,13 +7,15 @@ import 'package:ribn_toolkit/constants/styles.dart';
 
 /// A section to display important warning message when restoring wallet.
 class WarningSection extends StatelessWidget {
-  const WarningSection({Key? key}) : super(key: key);
+  const WarningSection({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 309,
-      height: 165,
+      width: kIsWeb ? 509 : 309,
+      height: kIsWeb ? 185 : 165,
       decoration: const BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(4)),
         color: RibnColors.greyTransparent,
@@ -26,7 +29,12 @@ class WarningSection extends StatelessWidget {
             children: [
               Text(
                 Strings.warning,
-                style: RibnToolkitTextStyles.extH3.copyWith(color: Colors.white),
+                style: kIsWeb
+                    ? RibnToolkitTextStyles.onboardingH3.copyWith(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 19.5,
+                      )
+                    : RibnToolkitTextStyles.extH3.copyWith(color: Colors.white),
               ),
               const SizedBox(
                 width: 4,
@@ -42,11 +50,13 @@ class WarningSection extends StatelessWidget {
           ),
           Center(
             child: SizedBox(
-              width: 270,
-              height: 102,
+              width: kIsWeb ? 441 : 270,
+              height: kIsWeb ? 92 : 102,
               child: Text(
                 Strings.restoreWalletWarning,
-                style: RibnToolkitTextStyles.smallBody.copyWith(fontSize: 15, color: Colors.white),
+                style: kIsWeb
+                    ? RibnToolkitTextStyles.onboardingH3
+                    : RibnToolkitTextStyles.smallBody.copyWith(fontSize: 15, color: Colors.white),
               ),
             ),
           ),
