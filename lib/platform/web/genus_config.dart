cf5295b876b536461a9fe7ad06f65bd870455fd2
Rex Serubii <selby@serubii.com>
2022-10-26 00:10:00 -0300

    Extracted Private IP to common variable in network_utils

--- lib/platform/web/genus_config.dart
+++ lib/platform/web/genus_config.dart
@@ -1,5 +1,7 @@
 import 'package:grpc/grpc_web.dart';
+import 'package:ribn/constants/network_utils.dart';
+
 
 class PlatformGenusConfig {
-  static GrpcWebClientChannel channel = GrpcWebClientChannel.xhr(Uri.parse('http://35.226.176.100:8099'));
+  static GrpcWebClientChannel channel = GrpcWebClientChannel.xhr(Uri.parse('http://${NetworkUtils.privateIP}:8099'));
 }
