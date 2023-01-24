d410c582306b0e3fc2aedb81b212f88f80d72850
Siphamandla Mjoli <g14m1190@campus.ru.ac.za>
2023-01-24 17:05:59 +0200

    update(stash): stashing

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,10 @@ import 'dart:convert';
 
 // Flutter imports:
 import 'package:flutter/material.dart';
-
 // Package imports:
 import 'package:flutter_portal/flutter_portal.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
-
 // Project imports:
 import 'package:ribn/actions/internal_message_actions.dart';
 import 'package:ribn/constants/keys.dart';
@@ -34,7 +32,8 @@ import 'package:ribn/router/root_router.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Redux.initStore(initTestStore: true);
-  final AppViews currentAppView = await PlatformUtils.instance.getCurrentAppView();
+  final AppViews currentAppView =
+      await PlatformUtils.instance.getCurrentAppView();
   final bool needsOnboarding = Redux.store!.state.needsOnboarding();
   // Open app in new tab if user needs onboarding
   if (currentAppView == AppViews.extension && needsOnboarding) {
@@ -67,7 +66,8 @@ class RibnApp extends StatelessWidget {
           title: 'Ribn',
           navigatorObservers: [Routes.routeObserver],
           onGenerateRoute: rootRouter.generateRoutes,
-          onGenerateInitialRoutes: (initialRoute) => onGenerateInitialRoute(initialRoute, store),
+          onGenerateInitialRoutes: (initialRoute) =>
+              onGenerateInitialRoute(initialRoute, store),
           initialRoute: getInitialRoute(store),
           navigatorKey: Keys.navigatorKey,
         ),
@@ -90,9 +90,11 @@ String getInitialRoute(Store<AppState> store) {
   //v2
   else if (store.state.internalMessage?.method == InternalMethods.authorize) {
     return Routes.connectDApp;
-  } else if (store.state.internalMessage?.method == InternalMethods.getBalance) {
+  } else if (store.state.internalMessage?.method ==
+      InternalMethods.getBalance) {
     return Routes.reviewAndSignDApp;
-  } else if (store.state.internalMessage?.method == InternalMethods.signTransaction) {
+  } else if (store.state.internalMessage?.method ==
+      InternalMethods.signTransaction) {
     return Routes.reviewAndSignDApp;
   }
 
@@ -129,7 +131,8 @@ List<Route> onGenerateInitialRoute(initialRoute, Store<AppState> store) {
     case Routes.externalSigning:
       return [
         MaterialPageRoute(
-          builder: (context) => ExternalSigningPage(store.state.internalMessage!),
+          builder: (context) =>
+              ExternalSigningPage(store.state.internalMessage!),
           settings: const RouteSettings(name: Routes.externalSigning),
         )
       ];
@@ -170,11 +173,13 @@ Future<void> initBgConnection(Store<AppState> store) async {
   try {
     Messenger.instance.connect();
     Messenger.instance.initMsgListener((String msgFromBgScript) {
-      final InternalMessage pendingRequest = InternalMessage.fromJson(msgFromBgScript);
+      final InternalMessage pendingRequest =
+          InternalMessage.fromJson(msgFromBgScript);
       store.dispatch(ReceivedInternalMsgAction(pendingRequest));
       completer.complete();
     });
-    Messenger.instance.sendMsg(jsonEncode({'method': InternalMethods.checkPendingRequest}));
+    Messenger.instance
+        .sendMsg(jsonEncode({'method': InternalMethods.checkPendingRequest}));
   } catch (e) {
     completer.complete();
     PlatformUtils.instance.closeWindow();
