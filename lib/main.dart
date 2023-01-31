3e425529ae9b66781c805783a6540d305348c498
Siphamandla Mjoli <g14m1190@campus.ru.ac.za>
2023-01-31 07:48:11 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'dart:convert';
 
 // Flutter imports:
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 // Package imports:
 import 'package:flutter_portal/flutter_portal.dart';
