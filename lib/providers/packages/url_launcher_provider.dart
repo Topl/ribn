28e54b5ac2ad79f2755bb716df432509801d7e68
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-02-24 09:07:27 -0600

    Symantic(urlLauncher): Add explicit types

--- lib/providers/packages/url_launcher_provider.dart
+++ lib/providers/packages/url_launcher_provider.dart
@@ -1,6 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-final urlLauncherProvider = Provider<Future<bool> Function(Uri)>((ref) {
+final Provider<Future<bool> Function(Uri)> urlLauncherProvider =
+    Provider<Future<bool> Function(Uri)>((ref) {
   return (Uri uri) => launchUrl(uri);
 });
