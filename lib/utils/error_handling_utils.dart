147d84cbfbfd8abefd03f7e69616d0cf561e01ea
Selby Baidjnath <selby@serubii.com>
2023-03-02 14:22:55 -0300

    remove unecessary import for CI

--- lib/utils/error_handling_utils.dart
+++ lib/utils/error_handling_utils.dart
@@ -1,5 +1,4 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:ribn/constants/loggers.dart';
 import 'package:ribn/constants/routes.dart';
 import 'package:ribn/providers/logger_provider.dart';
 import 'package:ribn/utils/navigation_utils.dart';
