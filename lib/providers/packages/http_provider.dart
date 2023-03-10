3535a7a2c0433ec580917d910ea94461ec4aa1a5
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-03-10 07:15:26 -0800

    Http service

--- lib/providers/packages/http_provider.dart
+++ lib/providers/packages/http_provider.dart
@@ -1,6 +1,11 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:http/http.dart' as http;
 
-final httpProvider = Provider<http.Request Function()>((ref) {
-  return () => http.Request;
+final httpClientProvider = Provider.autoDispose<http.Client>((ref) {
+  return http.Client();
 });
+final httpMultipartProvider = Provider<http.MultipartRequest Function()>((ref) {
+  return () => http.MultipartRequest("POST", Uri.parse(kUploadFileURL));
+});
+
+final httpServiceProvider = Provider<HttpService>((ref) => HttpService(ref.read));
