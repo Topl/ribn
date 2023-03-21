b0839661604860642e945d1e0596ae63dda4d1bd
Selby Baidjnath <selby@serubii.com>
2023-03-21 20:05:57 -0300

    Added supporting helpers, extensions and builder required for eventInstantiation

--- lib/providers/analytics/analytics_provider.dart
+++ lib/providers/analytics/analytics_provider.dart
@@ -1,10 +1,8 @@
 // Dart imports:
 import 'dart:async';
-import 'dart:io' show Platform;
 
 // Package imports:
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
@@ -13,6 +11,7 @@ import 'package:ribn/platform/platform.dart';
 import 'package:ribn/providers/logger_provider.dart';
 import 'package:ribn/providers/packages/flutter_secure_storage_provider.dart';
 import 'package:ribn/utils/extensions.dart';
+import 'package:ribn/utils/platform_utils.dart';
 
 final analyticsProvider = StateNotifierProvider<AnalyticsNotifier, AsyncValue<AnalyticsState>>((ref) {
   return AnalyticsNotifier(ref);
@@ -28,12 +27,14 @@ class AnalyticsNotifier extends StateNotifier<AsyncValue<AnalyticsState>> {
     _isAnalyticsEnabled().then((isEnabled) {
       state = AsyncData(AnalyticsState(isEnabled: isEnabled));
       if (isEnabled) {
-        _service = FirebaseAnalyticsService();
+        _service = FirebaseAnalyticsService(ref);
       }
     });
   }
 
-  void log(String name, Map<String, dynamic> parameters) => _service.logEvent(name, parameters);
+  void log(String name, Map<String, dynamic> parameters) => _service.logCustomEvent(name, parameters);
+
+  void logWithEnum(AnalyticsEvents event, AnalyticsEventData data) => _service.logEventFromEnum(event, data);
 
   static const _analyticsEnabledKey = "biometricsEnabled";
 
@@ -61,7 +62,7 @@ class AnalyticsNotifier extends StateNotifier<AsyncValue<AnalyticsState>> {
         override: ref.read(flutterSecureStorageProvider)());
 
     if (isEnabled) {
-      _service = FirebaseAnalyticsService();
+      _service = FirebaseAnalyticsService(ref);
       logger.log(
         logLevel: LogLevel.Info,
         loggerClass: LoggerClass.Analytics,
@@ -81,31 +82,53 @@ class AnalyticsNotifier extends StateNotifier<AsyncValue<AnalyticsState>> {
 
   Future<bool> _isAnalyticsEnabled() async {
     return (await PlatformLocalStorage.instance
-        .getKVInSecureStorage(_analyticsEnabledKey, override: ref.read(flutterSecureStorageProvider)()))
+            .getKVInSecureStorage(_analyticsEnabledKey, override: ref.read(flutterSecureStorageProvider)()))
         .toBooleanWithNullableDefault(false);
   }
 }
 
 abstract class AnalyticsService {
-  void logEvent(String name, Map<String, dynamic> parameters);
+  AnalyticsService();
+
+  void logCustomEvent(String name, Map<String, dynamic> parameters);
+
+  Future<void> logEventFromEnum(AnalyticsEvents event, AnalyticsEventData data);
 }
 
 class voidAnalyticsService implements AnalyticsService {
-  void logEvent(String name, Map<String, dynamic> parameters) {
+  voidAnalyticsService();
+
+  void logCustomEvent(String name, Map<String, dynamic> parameters) {
+    // do nothing
+  }
+
+  @override
+  Future<void> logEventFromEnum(AnalyticsEvents event, AnalyticsEventData data) async {
     // do nothing
   }
 }
 
 class FirebaseAnalyticsService implements AnalyticsService {
   FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  final Ref ref;
+
+  FirebaseAnalyticsService(this.ref);
 
   /***
    * Logs an event to firebase analytics
    * Use the AnalyticsEventBuilders to build the parameters
    */
-  Future<void> logEvent(String name, Map<String, dynamic> parameters) async {
+  Future<void> logCustomEvent(String name, Map<String, dynamic> parameters) async {
     // log event to firebase
     await FirebaseAnalytics.instance.logEvent(name: name, parameters: parameters);
+  }
+
+  /** Logs an event to firebase analytics
+   * Use the [AnalyticsEventBuilders] to build the [AnalyticsEventData] parameters
+   */
+  Future<void> logEventFromEnum(AnalyticsEvents event, AnalyticsEventData data) async {
+    // log event to firebase
+    await FirebaseAnalytics.instance.logEvent(name: event.name, parameters: data.toMap());
   }
 }
 
@@ -127,25 +150,66 @@ enum AnalyticsEvents {
   final String name;
 }
 
+// This class needs to be in the same file as [AnalyticsEventBuilders] due to the private constructor
+class AnalyticsEventData {
+  final AnalyticsEvents event;
+  final Map<String, dynamic> _value;
+
+  // Private constructor
+  AnalyticsEventData._(this.event, this._value);
+
+  //to map
+  Map<String, dynamic> toMap() => _value;
+}
+
 class AnalyticsEventBuilders {
-  static Map<String, dynamic> buildAbandonTransactionEvent() {
-    return {
-      'event': AnalyticsEvents.AbandonTransactionEvent.toString(),
-      'timestamp': DateTime
-          .now()
-          .millisecondsSinceEpoch,
-      'platform': getPlatform(),
-    };
+  Ref ref;
+
+  AnalyticsEventBuilders(this.ref);
+
+  AnalyticsEventData buildAbandonTransactionEvent(AnalyticsEvents event,
+      {int contractInteractions = 0, assetType = 'Unknown', double transferAmount = 0}) {
+    return AnalyticsEventData._(
+        event,
+        _optionsBuilder(walletAddress: true, userType: true, event: event, parameters: {
+          "contractInteractions": contractInteractions,
+          "assetType": assetType,
+          "transferAmount": transferAmount,
+        }));
   }
 
+  static Map<String, dynamic> _optionsBuilder(
+      {bool defaultMetrics = true,
+      bool eventName = true,
+      bool walletAddress = false,
+      bool userType = false,
+      required AnalyticsEvents event,
+      Map<String, dynamic> parameters = const {}}) {
+    return parameters
+        .addIf(eventName, _addEventName(event))
+        .addIf(defaultMetrics, _addDefaultMetrics())
+        .addIf(walletAddress, _addWalletAddress())
+        .addIf(userType, _addUserType());
+  }
 
-  static String getPlatform() =>
-      kIsWeb ? "Web" :
-      Platform.isIOS ? "IOS" :
-      Platform.isAndroid ? "Android" :
-      Platform.isMacOS ? "MacOS" :
-      Platform.isWindows ? "Windows" :
-      Platform.isLinux ? "Linux" :
-      Platform.isFuchsia ? "Fuchsia" :
-      "Unknown";
+  static Map<String, dynamic> _addDefaultMetrics({Map<String, dynamic> parameters = const {}}) => {
+        'timestamp': DateTime.now().millisecondsSinceEpoch,
+        'platform': getPlatform(),
+        ...parameters,
+      };
+
+  static Map<String, dynamic> _addWalletAddress({Map<String, dynamic> parameters = const {}}) => {
+        'walletAddress': 'TODO: PLACEHOLDER'.toHashSha256(),
+        ...parameters,
+      };
+
+  static Map<String, dynamic> _addUserType({Map<String, dynamic> parameters = const {}}) => {
+        'userType': 'TODO: PLACEHOLDER',
+        ...parameters,
+      };
+
+  static Map<String, dynamic> _addEventName(AnalyticsEvents event, {Map<String, dynamic> parameters = const {}}) => {
+        'event': event.name.toString(),
+        ...parameters,
+      };
 }
