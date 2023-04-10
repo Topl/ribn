3460b4b484e25c184352a926446ba1b96cedf425
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-04-10 10:03:42 -0600

    First commit

---
+++ lib/v2/view/screens/asset_managment/asset_managment_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AssetManagementScreen extends StatelessWidget {
+  static const Key assetManagementScreenKey = Key('assetManagementScreenKey');
+  const AssetManagementScreen({
+    Key key = assetManagementScreenKey,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
