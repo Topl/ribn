cd18c8f0ca4fe7ad1102b6d771d96b458a29f01c
Kelz <makamanekj@gmail.com>
2023-08-24 15:17:22 +0200

    feat(ANNULUS-254): Send assets

--- lib/v2/asset_managment/widgets/asset_screen_header/asset_screen_header.dart
+++ lib/v2/asset_managment/widgets/asset_screen_header/asset_screen_header.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:ribn/v2/shared/constants/colors.dart';
+import 'package:ribn/v2/shared/constants/strings.dart';
 
 class AssetScreenHeader extends StatelessWidget {
   const AssetScreenHeader({
@@ -13,10 +14,36 @@ class AssetScreenHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: RibnColors.grey,
-      child: Center(
-        child: Text(
-          'Asset Header',
-        ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: RibnColors.grey,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      Strings.totalBalance,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    Text(
+                      '0.00',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              Strings.assetHeader,
+            ),
+          ),
+        ],
       ),
     );
   }
