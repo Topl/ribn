5fbba3fc8759c98b149c2d3f7a3d764fd9690915
lukorito <kelvin@stitch.money>
2023-08-30 15:01:48 +0300

    implement feedback

--- lib/v2/asset_managment/providers/transactions/keychain_network_providers/assets_provider_class.dart
+++ lib/v2/asset_managment/providers/transactions/keychain_network_providers/assets_provider_class.dart
@@ -4,7 +4,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 // Project imports:
 import 'package:ribn/v2/asset_managment/models/asset_details.dart';
 
-// TODO SDK - Should this even be called transactions? Maybe it should be called crypto
 abstract class AssetsNotifier extends StateNotifier<AsyncValue<List<AssetDetails>>> {
   AssetsNotifier() : super(AsyncLoading()) {
     getAssets().then((value) {
